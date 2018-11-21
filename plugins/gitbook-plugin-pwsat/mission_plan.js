a04e561a591c3fa48715cbac72904d74f539580e
Piotr Kuligowski <xsaperx@gmail.com>
2018-11-21 19:19:28 +0100

    generator for sessions table with active links

---
+++ plugins/gitbook-plugin-pwsat/mission_plan.js
@@ -0,0 +1,40 @@
+const subpages = require('./subpages');
+const fs = require('fs');
+
+const MISSION_PLAN_PATH = "mission_plan/progress.json";
+const MISSION_PLAN_KEY = 'Mission Plan';
+
+function extractPhaseId(phaseMarkdownLink){
+    return phaseMarkdownLink.split("/").slice(-2)[0];
+}
+
+function readMissionPlan() {
+    const missionPlan = JSON.parse(fs.readFileSync(MISSION_PLAN_PATH))[MISSION_PLAN_KEY];
+
+    const phaseMarkdownLinks = [];
+    const phasesProgress = [];
+    const phaseIds = [];
+
+    for (let phase of Object.keys(missionPlan)) {
+        phaseIds.push(extractPhaseId(phase));
+        phaseMarkdownLinks.push(phase);
+        phasesProgress.push(missionPlan[phase]);
+    }
+
+    return [phaseIds, phaseMarkdownLinks, phasesProgress];
+}
+
+
+module.exports = {
+    getPhaseMarkdownLink: function(requestedPhaseId) {
+        const phaseIds = readMissionPlan()[0];
+
+        for (let id = 0; id < phaseIds.length; id++) {
+            if (requestedPhaseId === phaseIds[id]) {
+                return readMissionPlan()[1][id];
+            }
+        }
+
+        return requestedPhaseId;
+    }
+}
--- plugins/gitbook-plugin-pwsat/sessions.js
+++ plugins/gitbook-plugin-pwsat/sessions.js
@@ -1,24 +1,31 @@
 const subpages = require('./subpages');
+const session_data = require('./session_data');
 
-function buildSessions(pathToRead, items_filtered) {
-    markdownLinks = subpages.buildMarkdownLinks(pathToRead, items_filtered);
-    pagesPaths = subpages.buildSubpagesLinks(pathToRead, items_filtered);
+function buildSessions(resolvedPathToSessions, items_filtered) {
+    markdownLinks = subpages.buildMarkdownLinks(resolvedPathToSessions, items_filtered);
+    sessionPaths = subpages.buildSubpagesPaths(resolvedPathToSessions, items_filtered);
 
     sessions = [];
     for (let index = 0; index < markdownLinks.length; index++) {
-        const singleLink = markdownLinks[index];
-        sessions.push(`|${singleLink}|MISSING|MISSING|MISSING|MISSING|`);
+        const sessionDir = resolvedPathToSessions + "\\" + (index + 1).toString() + "\\";
+
+        const sessionStartTime =  session_data.getStartTime(sessionDir);
+        const sessionPhase = session_data.getPhase(sessionDir);
+        const sessionStatus = session_data.getStatus(sessionDir);
+        const sessionDescription = session_data.getShortDescription(sessionDir);
+
+        sessions.push(`|${markdownLinks[index]}|${sessionStartTime}|${sessionPhase}|${sessionStatus}|${sessionDescription}|`);
     }
-    return sessions;
+    return sessions.reverse();
 }
 
-function buildTable(pathToRead, items_filtered) {
+function buildTable(resolvedPathToSessions, items_filtered) {
     var result = [];
     result = [
         ...result,
-        `|**Index**|**Time**|**Phase**|**Status**|**Short description**|`,
-        `|-|-|-|-|-|`,
-        ...buildSessions(pathToRead, items_filtered),
+        `|**Index**|**Start Time**|**Phase**|**Status**|**Short description**|`,
+        `|:-:|:-:|:-:|:-:|:-|`,
+        ...buildSessions(resolvedPathToSessions, items_filtered),
         ''
     ];
     return result;
@@ -26,16 +33,16 @@ function buildTable(pathToRead, items_filtered) {
 
 module.exports = {
     process: function(block) {
-        const pathToRead = "sessions";
+        const pathToSessions = "sessions";
         const ignoredItems = ["index.md", "_templates", "_layout"];
 
         const ctx = this;
-        const items = subpages.getSubpages(ctx.resolve(pathToRead));
+        const items = subpages.getSubpages(ctx.resolve(pathToSessions));
 
         items_filtered = subpages.filterItems(items, ignoredItems);
 
         const links = [
-            ...buildTable(pathToRead, items_filtered)
+            ...buildTable(ctx.resolve(pathToSessions), items_filtered)
         ];
 
         return {
