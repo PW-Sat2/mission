2634bc295abce530c96b10a546882d51eccf16ca
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-06 17:55:00 +0100

    cam commissioning chunks

--- sessions/16/tasklist.py
+++ sessions/16/tasklist.py
@@ -38,11 +38,16 @@ tasks = [
     [tc.DownloadFile(115, '/suns_test_sec', [i for i in range(10, 20, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(116, '/suns_test_sec', [i for i in range(20, 30, 1)]), Send, WaitMode.Wait],
 
+    # Dark photos from camera commissioning
+    [tc.DownloadFile(176, 'cam_0.jpg', [0]), Send, WaitMode.Wait],
+    [tc.DownloadFile(177, 'cam_1.jpg', [0, 1, 8]), Send, WaitMode.Wait],
+
     [tc.SendBeacon(), Send, WaitMode.NoWait],
     [tc.DownloadFile(117, '/suns_test', [i for i in range(30, 40, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(118, '/suns_test', [i for i in range(40, 50, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(119, '/suns_test', [i for i in range(50, 60, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(120, '/suns_test', [i for i in range(60, 70, 1)]), Send, WaitMode.Wait],
+
     [tc.SendBeacon(), Send, WaitMode.NoWait],
     [tc.DownloadFile(121, '/suns_test', [i for i in range(70, 80, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(122, '/suns_test', [i for i in range(80, 90, 1)]), Send, WaitMode.Wait],
@@ -58,4 +63,6 @@ tasks = [
 
     [tc.DownloadFile(128, '/telemetry.previous', [i for i in range(1906, 2280, 25)]), Send, WaitMode.Wait],
     [tc.DownloadFile(129, '/telemetry.previous', [i for i in range(1909, 2280, 25)]), Send, WaitMode.Wait],
+
+    [tc.SendBeacon(), Send, WaitMode.NoWait],
 ]
