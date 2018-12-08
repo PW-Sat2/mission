f9abc57b582c703d8f0432bec1414f248d2da30e
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-08 08:53:13 +0100

    change telemetry current to previous

--- sessions/25/tasklist.py
+++ sessions/25/tasklist.py
@@ -6,9 +6,9 @@ tasks = [
     [tc.ListFiles(32, '/'), Send, WaitMode.Wait],
 
     # Telemetry between session 24 and now
-    [tc.DownloadFile(45, '/telemetry.current', [i for i in range(1200, 2200, 100)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(46, '/telemetry.current', [i for i in range(1233, 2200, 100)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(47, '/telemetry.current', [i for i in range(1266, 2200, 100)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(45, '/telemetry.previous', [i for i in range(1200, 2200, 100)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(46, '/telemetry.previous', [i for i in range(1233, 2200, 100)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(47, '/telemetry.previous', [i for i in range(1266, 2200, 100)]), Send, WaitMode.Wait],
 
     [tc.SetBitrate(39, 8), Send, WaitMode.Wait],
     [tc.PingTelecommand(), Send, WaitMode.Wait],
