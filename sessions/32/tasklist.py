a2379818f5120789d7d08bad4e6446c6a3dd2fdd
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-09 10:09:56 +0100

    remove files moved

--- sessions/32/tasklist.py
+++ sessions/32/tasklist.py
@@ -5,16 +5,6 @@ tasks = [
 
     [tc.ListFiles(250, '/'), Send, WaitMode.Wait],
 
-    # Deleting old commissioning files
-    [tc.RemoveFile(9, '/cam'), Send, WaitMode.Wait],
-    [tc.RemoveFile(10, '/cam_0.jpg'), Send, WaitMode.Wait],
-    [tc.RemoveFile(11, '/cam_1.jpg'), Send, WaitMode.Wait],
-    [tc.RemoveFile(12, '/pld_1_2.jpg'), Send, WaitMode.Wait],
-    [tc.RemoveFile(13, '/suns_test'), Send, WaitMode.Wait],
-    [tc.RemoveFile(14, '/suns_test_sec'), Send, WaitMode.Wait],
-
-    [tc.ListFiles(251, '/'), Send, WaitMode.Wait],
-
     # Telemetry between session 30 and 32
     [tc.DownloadFile(25, '/telemetry.previous', [i for i in range(1600, 2280, 50)]), Send, WaitMode.Wait],
     [tc.DownloadFile(26, '/telemetry.previous', [i for i in range(1608, 2280, 50)]), Send, WaitMode.Wait],
@@ -41,6 +31,16 @@ tasks = [
     [tc.DownloadFile(53, '/suns_1', [i for i in range(175, 200, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(54, '/suns_1', [i for i in range(200, 225, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(55, '/suns_1', [i for i in range(225, 250, 1)]), Send, WaitMode.Wait],
+
+    # Deleting old commissioning files
+    [tc.RemoveFile(9, '/cam'), Send, WaitMode.Wait],
+    [tc.RemoveFile(10, '/cam_0.jpg'), Send, WaitMode.Wait],
+    [tc.RemoveFile(11, '/cam_1.jpg'), Send, WaitMode.Wait],
+    [tc.RemoveFile(12, '/pld_1_2.jpg'), Send, WaitMode.Wait],
+    [tc.RemoveFile(13, '/suns_test'), Send, WaitMode.Wait],
+    [tc.RemoveFile(14, '/suns_test_sec'), Send, WaitMode.Wait],
+
+    [tc.ListFiles(251, '/'), Send, WaitMode.Wait],
 
     [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
 ]
