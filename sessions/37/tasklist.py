06878e279429bb7c7446ff130c8eb8e6175f5a83
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-10 00:34:53 +0100

    photo delay between 128 and 480 changed to 0

--- sessions/37/tasklist.py
+++ sessions/37/tasklist.py
@@ -10,19 +10,19 @@ tasks = [
 
     # Photos scheduled
     [tc.TakePhotoTelecommand(101, CameraLocation.Nadir, PhotoResolution.p128, 1, datetime.timedelta(minutes=1), 'p1_128'), Send, WaitMode.Wait],
-    [tc.TakePhotoTelecommand(102, CameraLocation.Nadir, PhotoResolution.p480, 1, datetime.timedelta(minutes=1), 'p1_480'), Send, WaitMode.Wait],
+    [tc.TakePhotoTelecommand(102, CameraLocation.Nadir, PhotoResolution.p480, 1, datetime.timedelta(minutes=0), 'p1_480'), Send, WaitMode.Wait],
 
     [tc.TakePhotoTelecommand(111, CameraLocation.Nadir, PhotoResolution.p128, 1, datetime.timedelta(minutes=2), 'p2_128'), Send, WaitMode.Wait],
-    [tc.TakePhotoTelecommand(112, CameraLocation.Nadir, PhotoResolution.p480, 1, datetime.timedelta(minutes=2), 'p2_480'), Send, WaitMode.Wait],
+    [tc.TakePhotoTelecommand(112, CameraLocation.Nadir, PhotoResolution.p480, 1, datetime.timedelta(minutes=0), 'p2_480'), Send, WaitMode.Wait],
 
     [tc.TakePhotoTelecommand(121, CameraLocation.Nadir, PhotoResolution.p128, 1, datetime.timedelta(minutes=2), 'p3_128'), Send, WaitMode.Wait],
-    [tc.TakePhotoTelecommand(122, CameraLocation.Nadir, PhotoResolution.p480, 1, datetime.timedelta(minutes=2), 'p3_480'), Send, WaitMode.Wait],
+    [tc.TakePhotoTelecommand(122, CameraLocation.Nadir, PhotoResolution.p480, 1, datetime.timedelta(minutes=0), 'p3_480'), Send, WaitMode.Wait],
 
     [tc.TakePhotoTelecommand(131, CameraLocation.Nadir, PhotoResolution.p128, 1, datetime.timedelta(minutes=2), 'p4_128'), Send, WaitMode.Wait],
-    [tc.TakePhotoTelecommand(132, CameraLocation.Nadir, PhotoResolution.p480, 1, datetime.timedelta(minutes=2), 'p4_480'), Send, WaitMode.Wait],
+    [tc.TakePhotoTelecommand(132, CameraLocation.Nadir, PhotoResolution.p480, 1, datetime.timedelta(minutes=0), 'p4_480'), Send, WaitMode.Wait],
 
     [tc.TakePhotoTelecommand(141, CameraLocation.Nadir, PhotoResolution.p128, 1, datetime.timedelta(minutes=2), 'p5_128'), Send, WaitMode.Wait],
-    [tc.TakePhotoTelecommand(142, CameraLocation.Nadir, PhotoResolution.p480, 1, datetime.timedelta(minutes=2), 'p5_480'), Send, WaitMode.Wait],
+    [tc.TakePhotoTelecommand(142, CameraLocation.Nadir, PhotoResolution.p480, 1, datetime.timedelta(minutes=0), 'p5_480'), Send, WaitMode.Wait],
 
     [tc.SetBitrate(93, 8), Send, WaitMode.Wait],
 
