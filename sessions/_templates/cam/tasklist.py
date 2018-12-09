a0993c1494501bb50c6c159596e441c8cb91582d
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-09 22:03:34 +0100

    cam template clean-up

--- sessions/_templates/cam/tasklist.py
+++ sessions/_templates/cam/tasklist.py
@@ -1,15 +1,13 @@
 tasks = [
-    [[tc.SetBitrate(5, 8), 3], SendLoop, WaitMode.NoWait],
+    [[tc.SetBitrate(5, 1), 3], SendLoop, WaitMode.NoWait],
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
     [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
 
-    # Telemetry between session 33 and 35
+    # Telemetry between session x and x
     [tc.DownloadFile(32, '/telemetry.current', [i for i in range(750, 1750, 50)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(33, '/telemetry.current', [i for i in range(775, 1750, 50)]), Send, WaitMode.Wait],
-
-    [tc.DownloadFile(34, '/telemetry.current', [i for i in range(762, 1750, 50)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(35, '/telemetry.current', [i for i in range(787, 1750, 50)]), Send, WaitMode.Wait],
     
+    [tc.SetBitrate(3, 8), Send, WaitMode.Wait],
+
     [tc.SendBeacon(), Send, WaitMode.Wait],
 ]
