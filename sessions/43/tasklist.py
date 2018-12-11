dd87875b3d96c1b0187c3d371473e3caefb08ddb
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-11 08:14:50 +0100

    radfet experiment before telemetry

--- sessions/43/tasklist.py
+++ sessions/43/tasklist.py
@@ -4,13 +4,13 @@ tasks = [
 
     [tc.ListFiles(201, '/'), Send, WaitMode.Wait],
 
-    # Telemetry download between session 41 and 43
-    [tc.DownloadFile(202, '/telemetry.current', [i for i in range(0, 1000, 100)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(203, '/telemetry.previous', [i for i in range(2100, 2280, 50)]), Send, WaitMode.Wait],
-
     # First RadFET experiment triggering
     [tc.PerformRadFETExperiment(13, 150, 110, 'radfet'), Send, WaitMode.Wait],
     [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    # Telemetry download between session 41 and 43
+    [tc.DownloadFile(202, '/telemetry.current', [i for i in range(0, 1000, 100)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(203, '/telemetry.previous', [i for i in range(2100, 2280, 50)]), Send, WaitMode.Wait],
 
     # Goodbye satellite with 9600
     [[tc.SetBitrate(204, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
