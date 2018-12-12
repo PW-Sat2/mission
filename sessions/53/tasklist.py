5a259fa23318b1568315e26672110d42d8c741fa
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-12 20:55:18 +0100

    session 53

--- sessions/53/tasklist.py
+++ sessions/53/tasklist.py
@@ -1,42 +1,39 @@
 tasks = [
-    [[tc.SetBitrate(1, BaudRate.BaudRate1200), 5], SendLoop, WaitMode.NoWait],
+    [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
     [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
 
-    # Telemetry download between session 50 and 52
-    [tc.DownloadFile(10, '/telemetry.previous', [i for i in range(1650, 2280, 100)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(11, '/telemetry.previous', [i for i in range(1700, 2280, 100)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(12, '/telemetry.current', [i for i in range(0, 600, 100)]), Send, WaitMode.Wait],
+    # Telemetry download between session 51 and 53
+    [tc.DownloadFile(12, '/telemetry.current', [i for i in range(0, 600, 25)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(13, '/telemetry.current', [i for i in range(12, 600, 25)]), Send, WaitMode.Wait],
 
-    # More telemetry between session 50 and 52
-    [tc.DownloadFile(20, '/telemetry.previous', [i for i in range(1466, 2280, 50)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(21, '/telemetry.previous', [i for i in range(1482, 2280, 50)]), Send, WaitMode.Wait],
-
-    [tc.DownloadFile(22, '/telemetry.current', [i for i in range(25, 600, 50)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(23, '/telemetry.current', [i for i in range(12, 600, 50)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(24, '/telemetry.current', [i for i in range(37, 600, 50)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(10, '/telemetry.previous', [i for i in range(1850, 2280, 25)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(11, '/telemetry.previous', [i for i in range(1862, 2280, 25)]), Send, WaitMode.Wait],
 
     # SunS experiment download
+    [tc.SendBeacon(), Send, WaitMode.NoWait],
     [tc.DownloadFile(25, '/suns_2', [i for i in range(0, 25, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(26, '/suns_2', [i for i in range(25, 50, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(27, '/suns_2', [i for i in range(50, 75, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(28, '/suns_2', [i for i in range(75, 100, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(29, '/suns_2', [i for i in range(100, 125, 1)]), Send, WaitMode.Wait],
+    [tc.SendBeacon(), Send, WaitMode.NoWait],
     [tc.DownloadFile(30, '/suns_2', [i for i in range(125, 150, 1)]), Send, WaitMode.Wait],
-    [tc.SendBeacon(), Send, WaitMode.Wait],
     [tc.DownloadFile(31, '/suns_2', [i for i in range(150, 175, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(32, '/suns_2', [i for i in range(175, 200, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(33, '/suns_2', [i for i in range(200, 225, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(34, '/suns_2', [i for i in range(225, 250, 1)]), Send, WaitMode.Wait],
 
     # Chunk by chunk telemetry during SunS Experiment
+    [tc.SendBeacon(), Send, WaitMode.NoWait],
     [tc.DownloadFile(40, '/telemetry.previous', [i for i in range(1650, 1850, 10)]), Send, WaitMode.Wait],
     [tc.DownloadFile(41, '/telemetry.previous', [i for i in range(1651, 1850, 10)]), Send, WaitMode.Wait],
     [tc.DownloadFile(42, '/telemetry.previous', [i for i in range(1652, 1850, 10)]), Send, WaitMode.Wait],
     [tc.DownloadFile(43, '/telemetry.previous', [i for i in range(1653, 1850, 10)]), Send, WaitMode.Wait],
     [tc.DownloadFile(44, '/telemetry.previous', [i for i in range(1654, 1850, 10)]), Send, WaitMode.Wait],
     [tc.DownloadFile(45, '/telemetry.previous', [i for i in range(1655, 1850, 10)]), Send, WaitMode.Wait],
+    [tc.SendBeacon(), Send, WaitMode.NoWait],
     [tc.DownloadFile(46, '/telemetry.previous', [i for i in range(1656, 1850, 10)]), Send, WaitMode.Wait],
     [tc.DownloadFile(47, '/telemetry.previous', [i for i in range(1657, 1850, 10)]), Send, WaitMode.Wait],
     [tc.DownloadFile(48, '/telemetry.previous', [i for i in range(1658, 1850, 10)]), Send, WaitMode.Wait],
