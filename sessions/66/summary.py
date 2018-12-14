97f8e4af5c428f0a3d2ed96d50fa7d04ae21d0f2
begreeen <michal.gumiela@gmail.com>
2018-12-14 20:46:02 +0100

    decoded artifacts from session 66

---
+++ sessions/66/summary.py
@@ -0,0 +1,13 @@
+extract_file('p9_128_0', also=[63, 65])
+extract_file('p7_128_0', also=[63])
+
+
+decode_photo('p14_480_0')
+decode_photo('p15_480_0')
+
+decode_photo('assembled/p9_128_0')
+decode_photo('assembled/p7_128_0')
+
+parse_experiment('suns_3')
+parse_experiment('suns_3_sec')
+
