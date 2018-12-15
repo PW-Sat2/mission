6e406e69a8bd5f49b06b40736c5528ee374e0d2b
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-15 21:45:19 +0100

    get persistent

--- sessions/74/tasklist.py
+++ sessions/74/tasklist.py
@@ -7,6 +7,9 @@ tasks = [
     # Set periodic
     [tc.SetPeriodicMessageTelecommand(correlation_id=5, interval_minutes=1, repeat_count=0, message='empty'), Send, WaitMode.Wait],
 
+    # Persistent state
+    [tc.GetPersistentState(), Send, WaitMode.Wait],
+
     # Telemetry between session 73 and 74
     [tc.DownloadFile(10, '/telemetry.current', [i for i in range(1450, 1700, 12)]), Send, WaitMode.Wait],
     [tc.DownloadFile(11, '/telemetry.current', [i for i in range(1456, 1700, 12)]), Send, WaitMode.Wait],
