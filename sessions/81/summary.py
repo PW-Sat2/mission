a2b2b8497d96efc0175f68293ba3f51c51f37ed9
begreeen <michal.gumiela@gmail.com>
2018-12-16 22:45:01 +0100

    decoded artifacts from session 81

---
+++ sessions/81/summary.py
@@ -0,0 +1,22 @@
+extract_file('p1_480_0', also=[78, 80])
+
+extract_file('p5_480_0', also=[80])
+
+extract_file('p6_480_0', also=[80])
+
+extract_file('p7_480_0', also=[80])
+
+extract_file('p8_480_0', also=[80])
+
+
+decode_photo('assembled/p5_480_0')
+
+decode_photo('assembled/p6_480_0')
+
+decode_photo('assembled/p7_480_0')
+
+decode_photo('assembled/p8_480_0')
+
+decode_photo('assembled/p1_480_0')
+
+parse_experiment('suns_4')
