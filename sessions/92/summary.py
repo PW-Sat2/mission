b6aa48d484c92c866f71afbdbef1a2ee57eb4ae0
Maciej Nowak <maciejt.nowak@gmail.com>
2018-12-18 21:18:29 +0100

    Session 92: photos

---
+++ sessions/92/summary.py
@@ -0,0 +1,15 @@
+# 2,3,4,5,17,18
+ALSO = [90, 89, 88]
+extract_file('p2_480_0', also=ALSO)
+extract_file('p3_480_0', also=ALSO)
+extract_file('p4_480_0', also=ALSO)
+extract_file('p5_480_0', also=ALSO)
+extract_file('p17_480_0', also=ALSO)
+extract_file('p18_480_0', also=ALSO)
+
+decode_photo('assembled/p2_480_0')
+decode_photo('assembled/p3_480_0')
+decode_photo('assembled/p4_480_0')
+decode_photo('assembled/p5_480_0')
+decode_photo('assembled/p17_480_0')
+decode_photo('assembled/p18_480_0')
