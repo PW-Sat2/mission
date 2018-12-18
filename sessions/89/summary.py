88982c0d1adc0dfe5dbcf6a9be53748ef0e89827
operator <sat_operator@future-processing.com>
2018-12-18 11:45:57 +0100

    Added summary.py for session #89

---
+++ sessions/89/summary.py
@@ -0,0 +1,8 @@
+decode_photo('p2_480_0', also=[88])
+decode_photo('p3_480_0', also=[88])
+decode_photo('p3_128_0', also=[87])
+decode_photo('p4_480_0', also=[88])
+decode_photo('p5_480_0', also=[88])
+decode_photo('p17_480_0')
+decode_photo('p18_480_0')
+
