977585f67a8d47226c003ab945097ef49a2001ae
operator <sat_operator@future-processing.com>
2018-12-20 09:37:19 +0100

    Missing summary from session 96

---
+++ sessions/96/summary.py
@@ -0,0 +1,6 @@
+extract_file('suns_5', also=[95])
+extract_file('suns_5_sec', also=[95])
+parse_experiment('radfet_4')
+parse_experiment('assembled/suns_5')
+parse_experiment('assembled/suns_5_sec')
+
