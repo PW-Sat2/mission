028db8713d3f2de2a376bc3136be1b287f6eccc7
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-20 18:22:52 +0100

    session 104

---
+++ sessions/104/tasklist.py
@@ -0,0 +1,34 @@
+tasks = [
+    [[tc.SetBitrate(1, BaudRate.BaudRate1200), 5], SendLoop, WaitMode.NoWait],
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    # ReadMemory, weird telecommand
+    [tc.ReadMemory(2, 0x8801b620, 248), Send, WaitMode.Wait], # scrubbing
+
+    # Power cycle EPS B
+    [tc.PowerCycleTelecommand(3), Send, WaitMode.Wait],
+    [tc.PingTelecommand(), Send, WaitMode.Wait],
+
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    # ReadMemory, weird telecommand
+    [tc.ReadMemory(4, 0x8801b620, 248), Send, WaitMode.Wait], # scrubbing
+
+    # Set 9600
+    [tc.SetBitrate(5, BaudRate.BaudRate9600), Send, WaitMode.Wait],
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    [tc.ListFiles(6, '/'), Send, WaitMode.Wait],
+
+    # Telemetry between session 102 and 104
+    [tc.DownloadFile(10, '/telemetry.previous', [i for i in range(2250, 2280, 2)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(11, '/telemetry.previous', [i for i in range(2251, 2280, 2)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(12, '/telemetry.current', [i for i in range(0, 900, 50)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(13, '/telemetry.current', [i for i in range(25, 900, 50)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(14, '/telemetry.current', [i for i in range(12, 900, 50)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(15, '/telemetry.current', [i for i in range(37, 900, 50)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(16, '/telemetry.current', [i for i in range(6, 900, 50)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(17, '/telemetry.current', [i for i in range(18, 900, 50)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(18, '/telemetry.current', [i for i in range(31, 900, 50)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(19, '/telemetry.current', [i for i in range(43, 900, 50)]), Send, WaitMode.Wait],
+]
