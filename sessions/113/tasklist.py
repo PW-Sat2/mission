617b732e23ad3f54286e0031ad9c0de8b3fb0466
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-21 22:26:39 +0100

    beacon wait

--- sessions/113/tasklist.py
+++ sessions/113/tasklist.py
@@ -1,6 +1,6 @@
 tasks = [
     [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
-    [tc.SendBeacon(), Send, WaitMode.NoWait],
+    [tc.SendBeacon(), Send, WaitMode.Wait],
 
     [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
 
