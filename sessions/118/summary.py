f3e34268b6cf2606d99a53f2c17dfd1ef56fdb2d
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-22 20:26:01 +0100

    session 118

---
+++ sessions/118/summary.py
@@ -0,0 +1,12 @@
+extract_file('suns_7_sec', also=[116, 117])
+
+decode_photo('p1_128_0')
+decode_photo('p2_128_0')
+decode_photo('p3_128_0')
+decode_photo('p4_128_0')
+decode_photo('p5_128_0')
+decode_photo('p6_128_0')
+decode_photo('p7_128_0')
+decode_photo('p8_128_0')
+decode_photo('p9_128_0')
+decode_photo('p10_128_0')
