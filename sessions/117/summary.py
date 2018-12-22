ef937c8cf1139cdd398a6597938fffe0cb6feeb0
begreeen <michal.gumiela@gmail.com>
2018-12-22 19:55:50 +0100

    decoded suns exp

--- sessions/117/summary.py
+++ sessions/117/summary.py
@@ -1,3 +1,7 @@
 dump_scrubbing_status(2)
+
 extract_file('suns_7', also=[116])
 extract_file('suns_7_sec', also=[116])
+
+parse_experiment('assembled/suns_7')
+parse_experiment('assembled/suns_7_sec')
