895ce0dd6dc5ede19a691a2abca3a0042f729023
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-22 13:12:58 +0100

    scrubbing

--- sessions/117/tasklist.py
+++ sessions/117/tasklist.py
@@ -2,6 +2,9 @@ tasks = [
     [[tc.SetBitrate(1, BaudRate.BaudRate1200), 5], SendLoop, WaitMode.NoWait],
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
+    # ReadMemory, weird telecommand
+    [tc.ReadMemory(2, 0x8801b620, 248), Send, WaitMode.Wait], # scrubbing
+
     # Power cycle EPS B
     [tc.PowerCycleTelecommand(3), Send, WaitMode.Wait],
     [tc.PingTelecommand(), Send, WaitMode.Wait],
