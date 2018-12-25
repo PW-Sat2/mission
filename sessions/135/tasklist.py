316238307bc1f7f975757f23a01a5a94680fc244
begreeen <michal.gumiela@gmail.com>
2018-12-25 18:18:18 +0100

    improved tasklist

--- sessions/135/tasklist.py
+++ sessions/135/tasklist.py
@@ -9,7 +9,7 @@ tasks = [
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
     # ReadMemory, weird telecommand
-    ["ReadMemory, weird telecommand", Print, WaitMode.NoWait],
+    #["ReadMemory, weird telecommand", Print, WaitMode.NoWait],
     [tc.ReadMemory(2, 0x8801b620, 248), Send, WaitMode.Wait], # scrubbing
 
     ["List files - 7 items expected", Print, WaitMode.NoWait],
@@ -24,31 +24,32 @@ tasks = [
     [tc.RawI2C(4, 0, 0x35, 1, [0xE0]), Send, WaitMode.Wait],
 
     # Ping - start pinging about 30 s after reboot
-    ["Ping - start pinging about 30 s after reboot", Print, WaitMode.NoWait],
+    #["Ping - start pinging about 30 s after reboot", Print, WaitMode.NoWait],
     [tc.PingTelecommand(), Send, WaitMode.Wait],
 
     # Wait for non-zero beacon
-    ["Request till non-zero beacon decoded", Print, WaitMode.NoWait],
+    #["Request till non-zero beacon decoded", Print, WaitMode.NoWait],
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
 
     # Set 9600
-    ["Set 9600", Print, WaitMode.NoWait],
+    #["Set 9600", Print, WaitMode.NoWait],
     [tc.SetBitrate(10, BaudRate.BaudRate9600), Send, WaitMode.Wait],
 
-    # Get beacon at 9600
-    ["Get beacon at 9600", Print, WaitMode.NoWait],
-    [tc.SendBeacon(), Send, WaitMode.Wait],
+    # Beacon loop at 9600 - time to adjust GRC
+    ["Beacon loop at 9600", Print, WaitMode.NoWait],
+
+    [[tc.SendBeacon(), 5], SendLoop, WaitMode.NoWait],
 
     # Telemetry.current between session 134 and 135
-    ["Telemetry current between sessions  134 and 135", Print, WaitMode.NoWait],
+    #["Telemetry current between sessions  134 and 135", Print, WaitMode.NoWait],
 
     [tc.SendBeacon(), Send, WaitMode.NoWait],
     [tc.DownloadFile(20, '/telemetry.current', [i for i in range(0, 380, 25)]), Send, WaitMode.Wait],
     [tc.DownloadFile(21, '/telemetry.current', [i for i in range(14, 380, 25)]), Send, WaitMode.Wait],
 
     # Telemetry previous from 1760 to 2280 chunks
-    ["Telemetry previous from 1760 to 2280 chunks", Print, WaitMode.NoWait],
+    #["Telemetry previous from 1760 to 2280 chunks", Print, WaitMode.NoWait],
 
     [tc.SendBeacon(), Send, WaitMode.NoWait],
     [tc.DownloadFile(30, '/telemetry.previous', [i for i in range(1760, 2280, 25)]), Send, WaitMode.Wait],
@@ -57,7 +58,7 @@ tasks = [
 
 
     # More telemetry.current between session 134 and 135
-    ["More telemetry current between sessions  134 and 135", Print, WaitMode.NoWait],
+    #["More telemetry current between sessions  134 and 135", Print, WaitMode.NoWait],
 
     [tc.SendBeacon(), Send, WaitMode.NoWait],
     [tc.DownloadFile(40, '/telemetry.current', [i for i in range(4, 380, 25)]), Send, WaitMode.Wait],
@@ -65,7 +66,7 @@ tasks = [
     [tc.DownloadFile(42, '/telemetry.current', [i for i in range(19, 380, 25)]), Send, WaitMode.Wait],
 
     # More telemetry previous from 1760 to 2280 chunks
-    ["More telemetry previous from 1760 to 2280 chunks", Print, WaitMode.NoWait],
+    #["More telemetry previous from 1760 to 2280 chunks", Print, WaitMode.NoWait],
 
     [tc.SendBeacon(), Send, WaitMode.NoWait],
     [tc.DownloadFile(50, '/telemetry.previous', [i for i in range(1774, 2280, 25)]), Send, WaitMode.Wait],
@@ -74,13 +75,19 @@ tasks = [
 
 
     # Remove radfet file
-    ["Remove file", Print, WaitMode.Wait],
+    #["Remove file", Print, WaitMode.Wait],
     [tc.RemoveFile(60, '/radfet_7'), Send, WaitMode.Wait],
     [tc.ListFiles(61, '/'), Send, WaitMode.Wait],
 
 
+    ["Becon send loop - next is previously not downloaded telemetry.previous", Print, WaitMode.NoWait],
+    [[tc.SendBeacon(), 5], SendLoop, WaitMode.NoWait],
+
+    # ================================================================================
+    # WE ARE GOING TO SAVE THE ENERY - DOWNLOADING TELEMETRY BELOW IS NOT RECOMMENDED!
+    # ================================================================================
+
     # Previously not downloaded telemetry.previous
-    ["Previously not downloaded telemetry.previous", Print, WaitMode.NoWait],
 
     [tc.SendBeacon(), Send, WaitMode.NoWait],
     [tc.DownloadFile(100, '/telemetry.previous', [317, 332, 340, 342, 344, 345, 346, 347, 348, 349, 350, 351, 352, 353, 354, 355, 356, 357, 358, 359]), Send, WaitMode.Wait],
@@ -104,5 +111,5 @@ tasks = [
     [tc.DownloadFile(118, '/telemetry.previous', [1488, 1489, 1490, 1491, 1492, 1493, 1494, 1495, 1496, 1497, 1498, 1499, 1517, 1540, 1542, 1544, 1545, 1546, 1547, 1548]), Send, WaitMode.Wait],
     [tc.DownloadFile(119, '/telemetry.previous', [1550, 1551, 1553, 1555, 1556, 1557, 1558, 1559, 1577, 1592]), Send, WaitMode.Wait],
 
-    [tc.SendBeacon(), Send, WaitMode.Wait],
+    [[tc.SendBeacon(), 5], SendLoop, WaitMode.NoWait],
 ]
