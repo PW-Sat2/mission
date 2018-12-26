ee6183e121cf8b6bb93f1a963419b826453feb63
begreeen <michal.gumiela@gmail.com>
2018-12-26 10:05:15 +0100

    wait for better communication option (becon loop)

--- sessions/139/tasklist.py
+++ sessions/139/tasklist.py
@@ -8,6 +8,9 @@ tasks = [
     [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
+    # wait for better communication
+    [[tc.SendBeacon(), 15], SendLoop, WaitMode.NoWait],
+
     [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
 
     # Telemetry between session 136 and 139
