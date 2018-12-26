e1440cc667cf20eba8523f9676ceb652b5984508
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-27 00:00:51 +0100

    session 144

---
+++ sessions/144/tasklist.py
@@ -0,0 +1,17 @@
+tasks = [
+    [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
+
+    [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
+
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
+
+    # Telemetry between session 142 and 144
+    [tc.DownloadFile(10, '/telemetry.current', [i for i in range(850, 2250, 100)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(10, '/telemetry.current', [i for i in range(900, 2250, 100)]), Send, WaitMode.Wait],
+
+    [tc.DownloadFile(10, '/telemetry.current', [i for i in range(875, 2250, 100)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(10, '/telemetry.current', [i for i in range(925, 2250, 100)]), Send, WaitMode.Wait],
+
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
+]
