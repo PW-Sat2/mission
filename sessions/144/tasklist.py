16c737621eca798c750925465ef2c89e654eddc0
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-27 00:04:36 +0100

    cid duplicates fixed

--- sessions/144/tasklist.py
+++ sessions/144/tasklist.py
@@ -8,10 +8,10 @@ tasks = [
 
     # Telemetry between session 142 and 144
     [tc.DownloadFile(10, '/telemetry.current', [i for i in range(850, 2250, 100)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(10, '/telemetry.current', [i for i in range(900, 2250, 100)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(11, '/telemetry.current', [i for i in range(900, 2250, 100)]), Send, WaitMode.Wait],
 
-    [tc.DownloadFile(10, '/telemetry.current', [i for i in range(875, 2250, 100)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(10, '/telemetry.current', [i for i in range(925, 2250, 100)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(12, '/telemetry.current', [i for i in range(875, 2250, 100)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(13, '/telemetry.current', [i for i in range(925, 2250, 100)]), Send, WaitMode.Wait],
 
     [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
 ]
