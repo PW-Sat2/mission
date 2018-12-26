357cae14ebe5616a23e685dc14cda4f3ae64ee59
begreeen <michal.gumiela@gmail.com>
2018-12-26 10:01:58 +0100

    fix in chunks

--- sessions/139/tasklist.py
+++ sessions/139/tasklist.py
@@ -20,14 +20,14 @@ tasks = [
 
     [tc.DownloadFile(103, '/telemetry.current', [i for i in range(531, 1965, 60)]), Send, WaitMode.Wait],
 
-    [tc.DownloadFile(104, '/telemetry.current', [i for i in range(537, 1965, 60)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(104, '/telemetry.current', [i for i in range(544, 1965, 60)]), Send, WaitMode.Wait],
 
 
     # Telemetry between session 136 and 138 - missing from previous session
-    [tc.DownloadFile(200, '/telemetry.current', [501, 502, 504, 505, 507, 508, 510, 511, 512, 513, 514, 519, 522, 523, 524, 550, 562, 575, 587, 600]), Send, WaitMode.Wait],
-    [tc.DownloadFile(201, '/telemetry.current', [612, 637, 650, 662, 687, 712, 737, 762, 800, 812, 825, 837, 862, 887, 912, 937, 950, 962, 987, 1000]), Send, WaitMode.Wait],
-    [tc.DownloadFile(202, '/telemetry.current', [1012, 1062, 1075, 1100, 1112, 1137, 1162, 1175, 1212, 1225, 1262, 1275, 1287, 1300, 1312, 1362, 1400, 1412, 1462, 1512]), Send, WaitMode.Wait],
-    [tc.DownloadFile(203, '/telemetry.current', [1550, 1562, 1575, 1587, 1600, 1612, 1625, 1637, 1650, 1662, 1687, 1712, 1737, 1762, 1775, 1812, 1862]), Send, WaitMode.Wait],
+    [tc.DownloadFile(200, '/telemetry.current', [501, 502, 504, 505, 507, 508, 510, 511, 512, 513, 514, 519, 522, 523, 524, 550, 562, 587, 600, 1935]), Send, WaitMode.Wait],
+    [tc.DownloadFile(201, '/telemetry.current', [612, 637, 656, 662, 687, 712, 737, 762, 800, 812, 825, 837, 850, 862, 887, 912, 937, 956, 962, 987, 1006]), Send, WaitMode.Wait],
+    [tc.DownloadFile(202, '/telemetry.current', [1012, 1034, 1066, 1075, 1100, 1112, 1137, 1150, 1162, 1212, 1218, 1262, 1275, 1280, 1306, 1318, 1400, 1412, 1462, 1512]), Send, WaitMode.Wait],
+    [tc.DownloadFile(203, '/telemetry.current', [1336, 1753, 1555, 1575, 1580, 1606, 1618, 1630, 1637, 1650, 1667, 1687, 1697, 1712, 1737, 1762, 1812, 1862]), Send, WaitMode.Wait],
 
 
     [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
