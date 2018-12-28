07b449e2e8b63a6083d5559bacacf147c7545455
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-29 00:19:49 +0100

    more beacons

--- sessions/156/tasklist.py
+++ sessions/156/tasklist.py
@@ -15,11 +15,14 @@ tasks = [
     [tc.DownloadFile(10, '/telemetry.previous', [i for i in range(1550, 2280, 100)]), Send, WaitMode.Wait],
     [tc.DownloadFile(11, '/telemetry.current', [i for i in range(0, 700, 100)]), Send, WaitMode.Wait],
 
+    [tc.SendBeacon(), Send, WaitMode.NoWait],
     [tc.DownloadFile(12, '/telemetry.previous', [i for i in range(1600, 2280, 100)]), Send, WaitMode.Wait],
     [tc.DownloadFile(13, '/telemetry.current', [i for i in range(50, 700, 100)]), Send, WaitMode.Wait],
 
+    [tc.SendBeacon(), Send, WaitMode.NoWait],
     [tc.DownloadFile(14, '/telemetry.previous', [i for i in range(1575, 2280, 100)]), Send, WaitMode.Wait],
     [tc.DownloadFile(15, '/telemetry.previous', [i for i in range(1625, 2280, 100)]), Send, WaitMode.Wait],
+    [tc.SendBeacon(), Send, WaitMode.NoWait],
     [tc.DownloadFile(16, '/telemetry.current', [i for i in range(25, 700, 100)]), Send, WaitMode.Wait],
     [tc.DownloadFile(17, '/telemetry.current', [i for i in range(75, 700, 100)]), Send, WaitMode.Wait],
 
