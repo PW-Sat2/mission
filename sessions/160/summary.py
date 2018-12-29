94642c6d8100b9d91dfa2282620e660d09266220
Maciej Nowak <maciejt.nowak@gmail.com>
2018-12-29 20:40:30 +0100

    Full sail experiment file

--- sessions/160/summary.py
+++ sessions/160/summary.py
@@ -1,12 +1,23 @@
-# extract_file('sail.photo_45', also=[158])
-# extract_file('sail.photo_55', also=[158])
-# extract_file('sail.photo_67', also=[158])
+def extract_sail():
+    import base64
 
-# decode_photo('assembled/sail.photo_45')
-# decode_photo('assembled/sail.photo_55')
-# decode_photo('assembled/sail.photo_67')
+    file_frames = only_type(frames, response_frames.FileSendSuccessFrame)
+    file_frames = filter(lambda f: f.correlation_id == 199, file_frames)
 
-extract_file('sail.exp', also=[158])
+    sail_frames = only_type(store.get_session(157).frames(['all']), response_frames.SailExperimentFrame)
+    sail_frames += file_frames
+    sail_frames = unique_seqs(sail_frames)
+    sail_frames = sorted(sail_frames, key=lambda f: f.seq())
+
+    print(sail_frames)
+
+    with session.open_artifact('assembled/sail.exp', 'wb') as f:
+        for frame in sail_frames:
+            f.write(ensure_string(frame.response))
+
+extract_sail()
+
+parse_experiment('assembled/sail.exp')
 
 photos = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55,  57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87]
 
