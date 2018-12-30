4c361c323283a0d7a01263e29f2c9c3b1230178e
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-30 10:02:29 +0100

    session 164

---
+++ sessions/164/summary.py
@@ -0,0 +1,7 @@
+PHOTO_SESSIONS = [158, 160, 161, 163]
+
+photos = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87]
+
+for p in photos:
+    extract_file('sail.photo_{}'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/sail.photo_{}'.format(p), output_file_name='assembled/sail_photo_{}.jpg'.format(p))
