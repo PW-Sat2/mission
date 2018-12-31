765678ea0d11f4c3b4674b38d4e61a0ba9474d04
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-31 14:16:07 +0100

    scrubbing

--- sessions/173/tasklist.py
+++ sessions/173/tasklist.py
@@ -2,6 +2,9 @@ tasks = [
     [[tc.SetBitrate(1, BaudRate.BaudRate1200), 5], SendLoop, WaitMode.NoWait],
     [[tc.SendBeacon(), 15], SendLoop, WaitMode.NoWait],
 
+    # ReadMemory, weird telecommands
+    [tc.ReadMemory(2, 0x8801b620, 248), Send, WaitMode.Wait], # scrubbing
+
     # Power cycle EPS B
     [tc.PowerCycleTelecommand(3), Send, WaitMode.Wait],
     [tc.PingTelecommand(), Send, WaitMode.Wait],
@@ -12,7 +15,7 @@ tasks = [
     [tc.SetBitrate(6, BaudRate.BaudRate9600), Send, WaitMode.Wait],
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
-    [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
+    [tc.ListFiles(7, '/'), Send, WaitMode.Wait],
 
     # Telemetry between session 171 and 173
     [tc.DownloadFile(10, '/telemetry.current', [i for i in range(0, 250, 12)]), Send, WaitMode.Wait],
