cdb10d4a11298a2763d8f6e9aa6db09496567e29
begreeen <michal.gumiela@gmail.com>
2019-01-01 12:06:39 +0100

    fix becons

--- sessions/177/tasklist.py
+++ sessions/177/tasklist.py
@@ -2,8 +2,6 @@ tasks = [
     [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
-    [tc.SendBeacon(), Send, WaitMode.Wait],
-
     # Remove SADS experiment files
     [tc.RemoveFile(10, '/sads.exp'), Send, WaitMode.NoWait],
     [tc.RemoveFile(11, '/sads.photo_wing'), Send, WaitMode.Wait],
