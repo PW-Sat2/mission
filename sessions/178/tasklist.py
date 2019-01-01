4cf3d8140e5b9b2cda572b732be2ca234d31a020
begreeen <michal.gumiela@gmail.com>
2019-01-01 18:17:25 +0100

    fixed tasklist

--- sessions/178/tasklist.py
+++ sessions/178/tasklist.py
@@ -2,15 +2,11 @@ tasks = [
     [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
-    # Remove SADS experiment files
-    [tc.RemoveFile(10, '/sads.exp'), Send, WaitMode.NoWait],
-    [tc.RemoveFile(11, '/sads.photo_wing'), Send, WaitMode.Wait],
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
 
-    # Remove SADS experiment files once again
-    [tc.RemoveFile(20, '/sads.exp'), Send, WaitMode.NoWait],
-    [tc.RemoveFile(21, '/sads.photo_wing'), Send, WaitMode.Wait],
-
-    [tc.ListFiles(100, '/'), Send, WaitMode.Wait],
+    [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
 
     [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
 ]
