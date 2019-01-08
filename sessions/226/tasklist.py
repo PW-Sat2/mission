b5a374f810865ae2e0c9d8f1085b51327e9d3423
Maciej Nowak <maciejt.nowak@gmail.com>
2019-01-08 18:19:21 +0100

    226: removed chunks downloaded in 222

--- sessions/226/tasklist.py
+++ sessions/226/tasklist.py
@@ -24,11 +24,9 @@ tasks = [
     [tc.ListFiles(5, '/'), Send, WaitMode.Wait],
 
     # auto-generated telemetry start
-    [tc.DownloadFile(30, '/telemetry.previous', [437, 487, 537, 587, 637, 687, 737, 787, 837, 887, 937, 987, 1037, 1087, 1137, 1187, 1237, 1287, 1337, 1387]), Send, WaitMode.Wait],
-    [tc.DownloadFile(31, '/telemetry.previous', [1437, 1487, 1537, 1587, 1637, 1687, 1737, 1787, 1837, 1887, 1937, 1987, 2037, 2087, 2137, 2187, 2237, 462, 512, 562]), Send, WaitMode.Wait],
+    [tc.DownloadFile(31, '/telemetry.previous', [1937, 1987, 2037, 2087, 2137, 2187, 2237]), Send, WaitMode.Wait],
     [tc.DownloadFile(32, '/telemetry.current', [7, 57, 107, 157, 207, 257, 307, 357, 407, 457, 507, 557, 607, 657, 707, 757, 807, 32, 82, 132]), Send, WaitMode.Wait],
-    [tc.DownloadFile(33, '/telemetry.previous', [612, 662, 712, 762, 812, 862, 912, 962, 1012, 1062, 1112, 1162, 1212, 1262, 1312, 1362, 1412, 1462, 1512, 1562]), Send, WaitMode.Wait],
-    [tc.DownloadFile(34, '/telemetry.previous', [1612, 1662, 1712, 1762, 1812, 1862, 1912, 1962, 2012, 2062, 2112, 2162, 2212, 2262, 450, 500, 550, 600, 650, 700]), Send, WaitMode.Wait],
+    [tc.DownloadFile(34, '/telemetry.previous', [1962, 2012, 2062, 2112, 2162, 2212, 2262, 450, 500, 550, 600, 650, 700]), Send, WaitMode.Wait],
     [tc.DownloadFile(35, '/telemetry.current', [182, 232, 282, 332, 382, 432, 482, 532, 582, 632, 682, 732, 782, 832, 20, 70, 120, 170, 220, 270]), Send, WaitMode.Wait],
     [tc.DownloadFile(36, '/telemetry.previous', [750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350, 1400, 1450, 1500, 1550, 1600, 1650, 1700]), Send, WaitMode.Wait],
     [tc.DownloadFile(37, '/telemetry.previous', [1750, 1800, 1850, 1900, 1950, 2000, 2050, 2100, 2150, 2200, 2250, 474, 524, 574, 624, 674, 724, 774, 824, 874]), Send, WaitMode.Wait],
