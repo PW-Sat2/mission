caa38d78913c4d35566b904c8b3f3f619f4b4990
begreeen <michal.gumiela@gmail.com>
2019-01-16 19:29:44 +0100

    corr id fix

--- sessions/276/tasklist.py
+++ sessions/276/tasklist.py
@@ -19,8 +19,8 @@ tasks = [
     [tc.ListFiles(5, '/'), Send, WaitMode.Wait],
 
     # missings from 273
-    [tc.DownloadFile(30, '/telemetry.previous', [1891, 1903, 1915, 1927, 1941, 1953, 1965, 1977, 1991, 2003, 2015, 2027, 2041, 2053, 2065, 2077]), Send, WaitMode.Wait],
-    [tc.DownloadFile(31, '/telemetry.previous', [2091, 2103, 2115, 2127, 2141, 2153, 2165, 2177, 2191, 2203, 2215, 2227, 2241, 2253, 2265, 2277]), Send, WaitMode.Wait],
+    [tc.DownloadFile(20, '/telemetry.previous', [1891, 1903, 1915, 1927, 1941, 1953, 1965, 1977, 1991, 2003, 2015, 2027, 2041, 2053, 2065, 2077]), Send, WaitMode.Wait],
+    [tc.DownloadFile(21, '/telemetry.previous', [2091, 2103, 2115, 2127, 2141, 2153, 2165, 2177, 2191, 2203, 2215, 2227, 2241, 2253, 2265, 2277]), Send, WaitMode.Wait],
     
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
