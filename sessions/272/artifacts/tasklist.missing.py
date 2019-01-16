1860bc02f175ce8a0f67db8afdb20885d2fe1987
Michał Gumiela <michal.gumiela@gmail.com>
2019-01-16 09:30:52 +0100

    272 - summary

---
+++ sessions/272/artifacts/tasklist.missing.py
@@ -0,0 +1,8 @@
+tasks = [
+[tc.DownloadFile(30, '/telemetry.previous', [1041, 1053, 1065, 1077, 1091, 1103, 1115, 1127, 1141, 1153, 1165, 1177, 1191, 1203, 1215, 1227, 1241]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/telemetry.previous', [1253, 1265, 1277, 1291, 1303, 1315, 1327, 1341, 1353, 1365, 1377, 1391, 1403, 1415, 1427, 1441, 1453]), Send, WaitMode.Wait],
+	[tc.DownloadFile(32, '/telemetry.previous', [1465, 1477, 1491, 1503, 1515, 1527, 1541, 1553, 1565, 1577, 1591, 1603, 1615, 1627, 1641, 1653, 1665]), Send, WaitMode.Wait],
+	[tc.DownloadFile(33, '/telemetry.previous', [1677, 1691, 1703, 1715, 1727, 1741, 1753, 1765, 1777, 1791, 1803, 1815, 1827, 1841, 1853, 1865, 1877]), Send, WaitMode.Wait],
+	[tc.DownloadFile(34, '/telemetry.previous', [1891, 1903, 1915, 1927, 1941, 1953, 1965, 1977, 1991, 2003, 2015, 2027, 2041, 2053, 2065, 2077]), Send, WaitMode.Wait],
+	[tc.DownloadFile(35, '/telemetry.previous', [2091, 2103, 2115, 2127, 2141, 2153, 2165, 2177, 2191, 2203, 2215, 2227, 2241, 2253, 2265, 2277]), Send, WaitMode.Wait]
+]
