e95e73d97ec134a1c7aabef4db3a3ca1fca4e0cb
begreeen <michal.gumiela@gmail.com>
2019-01-16 09:48:58 +0100

    missing colon

--- sessions/273/tasklist.py
+++ sessions/273/tasklist.py
@@ -34,7 +34,7 @@ tasks = [
     [tc.DownloadFile(32, '/telemetry.previous', [1465, 1477, 1491, 1503, 1515, 1527, 1541, 1553, 1565, 1577, 1591, 1603, 1615, 1627, 1641, 1653, 1665]), Send, WaitMode.Wait],
     [tc.DownloadFile(33, '/telemetry.previous', [1677, 1691, 1703, 1715, 1727, 1741, 1753, 1765, 1777, 1791, 1803, 1815, 1827, 1841, 1853, 1865, 1877]), Send, WaitMode.Wait],
     [tc.DownloadFile(34, '/telemetry.previous', [1891, 1903, 1915, 1927, 1941, 1953, 1965, 1977, 1991, 2003, 2015, 2027, 2041, 2053, 2065, 2077]), Send, WaitMode.Wait],
-    [tc.DownloadFile(35, '/telemetry.previous', [2091, 2103, 2115, 2127, 2141, 2153, 2165, 2177, 2191, 2203, 2215, 2227, 2241, 2253, 2265, 2277]), Send, WaitMode.Wait]
+    [tc.DownloadFile(35, '/telemetry.previous', [2091, 2103, 2115, 2127, 2141, 2153, 2165, 2177, 2191, 2203, 2215, 2227, 2241, 2253, 2265, 2277]), Send, WaitMode.Wait],
     #
 
     [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
