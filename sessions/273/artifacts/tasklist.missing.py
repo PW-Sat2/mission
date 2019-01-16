633ea7dd3b14d6ed3647f53e59f8e8f0d74ee51e
Michał Gumiela <michal.gumiela@gmail.com>
2019-01-16 11:07:33 +0100

    273 - summary

---
+++ sessions/273/artifacts/tasklist.missing.py
@@ -0,0 +1,4 @@
+tasks = [
+[tc.DownloadFile(30, '/telemetry.previous', [1891, 1903, 1915, 1927, 1941, 1953, 1965, 1977, 1991, 2003, 2015, 2027, 2041, 2053, 2065, 2077]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/telemetry.previous', [2091, 2103, 2115, 2127, 2141, 2153, 2165, 2177, 2191, 2203, 2215, 2227, 2241, 2253, 2265, 2277]), Send, WaitMode.Wait]
+]
