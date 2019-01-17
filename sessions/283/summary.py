65ccdbe95fad216929be44abc6fc4daae9563471
Michał Gumiela <michal.gumiela@gmail.com>
2019-01-17 14:03:16 +0100

    tasklist for session 283, update desc of session 284 and some success propaganda for previous sessions

---
+++ sessions/283/summary.py
@@ -0,0 +1,7 @@
+PHOTO_SESSIONS = [277]
+
+photos = [3]
+
+for p in photos:
+    extract_file('p{}_480_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_480_0'.format(p), output_file_name='assembled/p{}_480_0.jpg'.format(p))
