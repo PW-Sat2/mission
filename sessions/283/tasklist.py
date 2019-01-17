cbc96a5250866436c4702c7ca47d3d4f1c443e83
Michał Gumiela <michal.gumiela@gmail.com>
2019-01-17 16:55:51 +0100

    repeated correlation id fix

--- sessions/283/tasklist.py
+++ sessions/283/tasklist.py
@@ -46,8 +46,8 @@ tasks = [
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
     # focia 3 hi-res - missings
-    [tc.DownloadFile(30, '/p3_480_0', [28, 31, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]), Send, WaitMode.Wait],
-    [tc.DownloadFile(31, '/p3_480_0', [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]), Send, WaitMode.Wait],
+    [tc.DownloadFile(100, '/p3_480_0', [28, 31, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]), Send, WaitMode.Wait],
+    [tc.DownloadFile(100, '/p3_480_0', [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]), Send, WaitMode.Wait],
 
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
