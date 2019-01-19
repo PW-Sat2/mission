fc8f587481638b2492ac4ef1e7e4d4b69ab1e414
Piotr Kuligowski <xsaperx@gmail.com>
2019-01-19 20:03:22 +0100

    currrent moved

--- sessions/297/tasklist.py
+++ sessions/297/tasklist.py
@@ -17,6 +17,21 @@ tasks = [
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
     [tc.ListFiles(5, '/'), Send, WaitMode.Wait],
+    
+    # ======= In case if telemetry.current was not overloaded
+    # Skip to step 18 in case of error frames
+    [tc.DownloadFile(40, '/telemetry.current', [i for i in range(1100, 2280, 48)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(41, '/telemetry.current', [i for i in range(1124, 2280, 48)]), Send, WaitMode.Wait],
+    
+    [tc.DownloadFile(42, '/telemetry.current', [i for i in range(1112, 2280, 48)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(43, '/telemetry.current', [i for i in range(1136, 2280, 48)]), Send, WaitMode.Wait],
+    
+    [tc.DownloadFile(44, '/telemetry.current', [i for i in range(1106, 2280, 48)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(45, '/telemetry.current', [i for i in range(1118, 2280, 48)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(46, '/telemetry.current', [i for i in range(1130, 2280, 48)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(47, '/telemetry.current', [i for i in range(1142, 2280, 48)]), Send, WaitMode.Wait],
+
+    [tc.SendBeacon(), Send, WaitMode.Wait],
 
     # manualy-generated telemetry start
     [tc.DownloadFile(30, '/telemetry.previous', [i for i in range(1100, 2280, 48)]), Send, WaitMode.Wait],
@@ -33,18 +48,6 @@ tasks = [
     [tc.DownloadFile(38, '/telemetry.current', [i for i in range(0, 30, 6)]), Send, WaitMode.Wait],
 
     # manualy-generated telemetry end
-    
-    # ======= In case if telemetry.current was not overloaded
-    [tc.DownloadFile(40, '/telemetry.current', [i for i in range(1100, 2280, 48)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(41, '/telemetry.current', [i for i in range(1124, 2280, 48)]), Send, WaitMode.Wait],
-    
-    [tc.DownloadFile(42, '/telemetry.current', [i for i in range(1112, 2280, 48)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(43, '/telemetry.current', [i for i in range(1136, 2280, 48)]), Send, WaitMode.Wait],
-    
-    [tc.DownloadFile(44, '/telemetry.current', [i for i in range(1106, 2280, 48)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(45, '/telemetry.current', [i for i in range(1118, 2280, 48)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(46, '/telemetry.current', [i for i in range(1130, 2280, 48)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(47, '/telemetry.current', [i for i in range(1142, 2280, 48)]), Send, WaitMode.Wait],
 
     [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
 ]
