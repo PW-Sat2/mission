14bb9e9793e86cae383ebcacdd7068aa334558f0
Piotr Kuligowski <xsaperx@gmail.com>
2019-01-20 10:27:16 +0100

    summary

---
+++ sessions/301/summary.py
@@ -0,0 +1,7 @@
+PHOTO_SESSIONS = []
+
+photos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for p in photos:
+    extract_file('p{}_128_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_128_0'.format(p), output_file_name='assembled/p{}_128_0.jpg'.format(p))
