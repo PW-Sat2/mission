568f134163af501c60e04b38ce3026fe25fbf254
Maciej Nowak <maciejt.nowak@gmail.com>
2019-01-21 21:17:18 +0100

    Scrubbing update

--- sessions/304/summary.py
+++ sessions/304/summary.py
@@ -5,3 +5,6 @@ photos = [1, 2]
 for p in photos:
     extract_file('p{}_480_0'.format(p), also=PHOTO_SESSIONS)
     decode_photo('assembled/p{}_480_0'.format(p), output_file_name='assembled/p{}_480_0.jpg'.format(p))
+
+
+dump_scrubbing_status(2)
