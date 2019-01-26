169aceaa2c7cc094919ab8b14b3fbcb397c0537d
Michał Gumiela <michal.gumiela@gmail.com>
2019-01-26 11:59:24 +0100

    343 - summary

---
+++ sessions/343/artifacts/tasklist.missing.py
@@ -0,0 +1,11 @@
+tasks = [
+[tc.DownloadFile(30, '/p7_128_0', [0, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/p7_128_0', [19, 20, 21, 22, 23, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]), Send, WaitMode.Wait],
+	[tc.DownloadFile(32, '/p8_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]), Send, WaitMode.Wait],
+	[tc.DownloadFile(33, '/p8_128_0', [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]), Send, WaitMode.Wait],
+	[tc.DownloadFile(34, '/telemetry.current', [1337, 1387, 1393, 1417, 1443]), Send, WaitMode.Wait],
+	[tc.DownloadFile(35, '/p9_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]), Send, WaitMode.Wait],
+	[tc.DownloadFile(36, '/p9_128_0', [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]), Send, WaitMode.Wait],
+	[tc.DownloadFile(37, '/p10_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]), Send, WaitMode.Wait],
+	[tc.DownloadFile(38, '/p10_128_0', [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]), Send, WaitMode.Wait]
+]
