01c12728044ff9cce30dd5db0d6bc0012cf46600
operator <sat_operator@future-processing.com>
2019-01-26 21:18:38 +0100

    346 - summary

---
+++ sessions/346/artifacts/tasklist.missing.py
@@ -0,0 +1,8 @@
+tasks = [
+[tc.DownloadFile(30, '/p2_480_0', [60]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/p3_480_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]), Send, WaitMode.Wait],
+	[tc.DownloadFile(32, '/p3_480_0', [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]), Send, WaitMode.Wait],
+	[tc.DownloadFile(33, '/p3_480_0', [36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53]), Send, WaitMode.Wait],
+	[tc.DownloadFile(34, '/p3_480_0', [54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71]), Send, WaitMode.Wait],
+	[tc.DownloadFile(35, '/p3_480_0', [72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88]), Send, WaitMode.Wait]
+]
