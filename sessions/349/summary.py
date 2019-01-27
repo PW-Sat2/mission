41e9ff35c481e52ab83c95d76867c77c18b7a99b
Piotr Kuligowski <xsaperx@gmail.com>
2019-01-27 10:10:45 +0100

    session 349 summary

---
+++ sessions/349/summary.py
@@ -0,0 +1,11 @@
+PHOTO_SESSIONS = [343, 346]
+
+photos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for p in photos:
+    extract_file('p{}_128_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_128_0'.format(p), output_file_name='assembled/p{}_128_0.jpg'.format(p))
+
+for p in photos:
+    extract_file('p{}_480_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_480_0'.format(p), output_file_name='assembled/p{}_480_0.jpg'.format(p))
