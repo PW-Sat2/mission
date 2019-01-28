af65e1e0cfe0e09a8aa5d5c220e2e558270f8268
operator <sat_operator@future-processing.com>
2019-01-28 21:32:13 +0100

    359 - summary

--- sessions/359/artifacts/tasklist.missing.py
+++ sessions/359/artifacts/tasklist.missing.py
@@ -1,5 +1,3 @@
 tasks = [
-[tc.DownloadFile(30, '/telemetry.previous', [900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350, 1400]), Send, WaitMode.Wait],
-	[tc.DownloadFile(31, '/telemetry.previous', [1450, 1500, 1550, 1600, 1650, 1700, 1750, 1800, 1850, 2150, 2200]), Send, WaitMode.Wait],
-	[tc.DownloadFile(32, '/telemetry.current', [170, 220, 470, 520, 570, 620, 670, 720, 770, 820, 870, 920, 970]), Send, WaitMode.Wait]
+
 ]
