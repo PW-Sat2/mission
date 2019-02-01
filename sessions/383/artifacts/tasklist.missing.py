1206b58bf0dc7d489c93f9022fd11e334f0fd72b
operator <sat_operator@future-processing.com>
2019-02-01 21:49:17 +0100

    383 - summary

---
+++ sessions/383/artifacts/tasklist.missing.py
@@ -0,0 +1,4 @@
+tasks = [
+[tc.DownloadFile(30, '/suns_ps_1_sec', [117, 120, 122, 123, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/suns_ps_1_sec', [136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149]), Send, WaitMode.Wait]
+]
