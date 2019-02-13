28917000688da89c6d41c743546c97b2746f3201
Piotr Kuligowski <xsaperx@gmail.com>
2019-02-13 22:11:23 +0100

    session 466 missings added

--- sessions/469/tasklist.py
+++ sessions/469/tasklist.py
@@ -26,5 +26,10 @@ tasks = [
     [tc.DownloadFile(42, '/telemetry.current', [733, 783, 833, 883, 933, 983, 1033, 1083, 1133]), Send, WaitMode.Wait],
     # auto-generated telemetry end
 
+    # Session 469 missings
+    [tc.DownloadFile(130, '/telemetry.previous', [21, 71, 121, 171, 221, 271, 321, 371, 421, 471, 521, 571, 621, 671, 721, 771, 821]), Send, WaitMode.Wait],
+	[tc.DownloadFile(131, '/telemetry.previous', [871, 921, 971, 1021, 1071, 1121, 1171, 1221, 1271, 1309, 1321, 1371, 1421, 1471, 1521, 1559]), Send, WaitMode.Wait],
+	[tc.DownloadFile(132, '/telemetry.previous', [1571, 1621, 1659, 1671, 1709, 1721, 1759, 1771, 1809, 1821, 1859, 1871, 1909, 1921, 1959, 1971]), Send, WaitMode.Wait],
+    
     [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
 ]
