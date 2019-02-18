a7e34a07231f499cdf5de25572690fae661d706c
operator <sat_operator@future-processing.com>
2019-02-18 19:59:53 +0100

    497 - summary

---
+++ sessions/497/artifacts/tasklist.missing.py
@@ -0,0 +1,18 @@
+tasks = [
+[tc.DownloadFile(30, '/telemetry.previous', [1300, 1306, 1312, 1324, 1330, 1336, 1350, 1356, 1362, 1374, 1380, 1386, 1400, 1406, 1412, 1418, 1424, 1430]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/telemetry.previous', [1436, 1450, 1456, 1462, 1474, 1480, 1486, 1500, 1506, 1512, 1524, 1530, 1536, 1550, 1556, 1562, 1574]), Send, WaitMode.Wait],
+	[tc.DownloadFile(32, '/telemetry.previous', [1580, 1586, 1600, 1606, 1612, 1624, 1630, 1636, 1650, 1656, 1662, 1674, 1680, 1686, 1700, 1706, 1712]), Send, WaitMode.Wait],
+	[tc.DownloadFile(33, '/telemetry.previous', [1718, 1724, 1730, 1736, 1750, 1756, 1762, 1774, 1780, 1786, 1800, 1806, 1812, 1824, 1830, 1836, 1850]), Send, WaitMode.Wait],
+	[tc.DownloadFile(34, '/telemetry.previous', [1856, 1862, 1874, 1880, 1886, 1900, 1906, 1912, 1924, 1930, 1936, 1950, 1956, 1962, 1974, 1980, 1986]), Send, WaitMode.Wait],
+	[tc.DownloadFile(35, '/telemetry.previous', [2000, 2006, 2012, 2024, 2030, 2036, 2050, 2056, 2062, 2074, 2080, 2086, 2100, 2106, 2112, 2118, 2124]), Send, WaitMode.Wait],
+	[tc.DownloadFile(36, '/telemetry.previous', [2130, 2136, 2150, 2156, 2162, 2174, 2180, 2186, 2200, 2206, 2212, 2224, 2230, 2236, 2250, 2262, 2274]), Send, WaitMode.Wait],
+	[tc.DownloadFile(37, '/telemetry.current', [0, 6, 20, 26, 32, 44, 50, 56, 70, 76, 82, 94, 100, 106, 120, 132, 144, 150, 156, 170]), Send, WaitMode.Wait],
+	[tc.DownloadFile(38, '/telemetry.current', [182, 194, 200, 206, 220, 232, 244, 250, 256, 270, 276, 282, 294, 300, 306, 320, 326, 332, 344]), Send, WaitMode.Wait],
+	[tc.DownloadFile(39, '/telemetry.current', [350, 356, 370, 376, 382, 394, 400, 406, 420, 426, 432, 444, 450, 456, 470, 476, 482, 494, 500]), Send, WaitMode.Wait],
+	[tc.DownloadFile(40, '/telemetry.current', [506, 520, 526, 532, 538, 544, 550, 556, 570, 576, 582, 588, 594, 600, 606, 620, 626, 632, 638]), Send, WaitMode.Wait],
+	[tc.DownloadFile(41, '/telemetry.current', [644, 650, 656, 670, 676, 682, 688, 694, 700, 706, 720, 726, 732, 738, 744, 750, 756, 770, 776]), Send, WaitMode.Wait],
+	[tc.DownloadFile(42, '/telemetry.current', [782, 788, 794, 800, 806, 820, 826, 832, 844, 850, 856, 870, 876, 882, 888, 894, 900, 906, 920]), Send, WaitMode.Wait],
+	[tc.DownloadFile(43, '/telemetry.current', [926, 932, 944, 950, 956, 970, 976, 982, 988, 994, 1000, 1006, 1020, 1026, 1032, 1038, 1044, 1050, 1056]), Send, WaitMode.Wait],
+	[tc.DownloadFile(44, '/telemetry.current', [1070, 1076, 1082, 1088, 1094, 1100, 1106, 1120, 1126, 1132, 1138, 1144, 1150, 1156, 1170, 1176, 1182, 1188, 1194]), Send, WaitMode.Wait],
+	[tc.DownloadFile(45, '/telemetry.current', [1200, 1206, 1220, 1226, 1232, 1238, 1244, 1250, 1256, 1270, 1276, 1282, 1294, 1300, 1306, 1320, 1326, 1332, 1344]), Send, WaitMode.Wait]
+]
