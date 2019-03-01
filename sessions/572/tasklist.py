296ab89f5b6f6e1052533fc3f031b4fb4fb6d862
Piotr Kuligowski <xsaperx@gmail.com>
2019-03-01 23:30:00 +0100

    radfet 9

--- sessions/572/tasklist.py
+++ sessions/572/tasklist.py
@@ -9,7 +9,7 @@ tasks = [
     [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
 
     # 8th RadFET Experiment
-    [tc.PerformRadFETExperiment(10, 150, 110, 'radfet_8'), Send, WaitMode.Wait],
+    [tc.PerformRadFETExperiment(10, 150, 110, 'radfet_9'), Send, WaitMode.Wait],
     
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
