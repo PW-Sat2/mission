a0fcc98b84113e609b86a355951b1f5806c4f935
Grzegorz Gajoch <gajoch@gmail.com>
2019-03-04 13:17:48 +0000

    SunS missings

--- sessions/590/tasklist.py
+++ sessions/590/tasklist.py
@@ -35,5 +35,13 @@ tasks = [
     [tc.DownloadFile(38, '/telemetry.current', [1275, 1325, 1375, 1425, 1475, 1525, 1575, 1625, 1675, 1725, 1775]), Send, WaitMode.Wait],
     # auto-generated telemetry end
 
+    # SunS missings from session 587
+    [tc.DownloadFile(30, '/suns_ps_2', [0, 17, 33, 34, 36, 67, 94, 108, 136, 145, 146, 147, 149, 154, 155, 156, 157, 158, 159]), Send, WaitMode.Wait],
+    [tc.DownloadFile(31, '/suns_ps_2', [160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177]), Send, WaitMode.Wait],
+    [tc.DownloadFile(32, '/suns_ps_2', [178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195]), Send, WaitMode.Wait],
+    [tc.DownloadFile(33, '/suns_ps_2', [196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213]), Send, WaitMode.Wait],
+    [tc.DownloadFile(34, '/suns_ps_2', [214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231]), Send, WaitMode.Wait],
+    [tc.DownloadFile(35, '/suns_ps_2', [232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249]), Send, WaitMode.Wait],
+
     [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
 ]
