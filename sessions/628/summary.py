ba304f6613f02c9d6e2ff4784b5a1f7fa17731a4
begreeen <michal.gumiela@gmail.com>
2019-03-10 10:43:54 +0100

    fix in summary

--- sessions/628/summary.py
+++ sessions/628/summary.py
@@ -3,5 +3,5 @@ PHOTO_SESSIONS = []
 photos = [2, 3, 7]
 
 for p in photos:
-    extract_file('p{}_128_0'.format(p), also=PHOTO_SESSIONS)
+    extract_file('p{}_480_0'.format(p), also=PHOTO_SESSIONS)
     decode_photo('assembled/p{}_480_0'.format(p), output_file_name='assembled/p{}_480_0.jpg'.format(p))
