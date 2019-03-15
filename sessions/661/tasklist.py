7f095c867dabb1e9c60457f0f350700b77b75a66
begreeen <michal.gumiela@gmail.com>
2019-03-15 21:00:33 +0100

    manual session because of callsign tests

---
+++ sessions/661/tasklist.py
@@ -0,0 +1,11 @@
+tasks = [
+    [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
+
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
+
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    [[tc.SendBeacon(), 40], SendLoop, WaitMode.NoWait],
+]
