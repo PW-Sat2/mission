c09d0579ac64c2b209705cde432e9589a0fda6b3
Maciej Nowak <maciejt.nowak@gmail.com>
2019-03-17 22:58:54 +0100

    Session 688

--- sessions/688/tasklist.py
+++ sessions/688/tasklist.py
@@ -1,3 +1,23 @@
 tasks = [
-    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait]
+    # Generated on 2019-03-17 22:58:37.575000
+
+    # Wake-up from deep-sleep
+    [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
+
+    # Get few beacons from nominal OBC
+    [[tc.SendBeacon(), 10], SendLoop, WaitMode.NoWait],
+
+    [tc.ListFiles(5, '/'), Send, WaitMode.Wait],
+
+    # Just waste some session time
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    ["Set bootslots for deep_sleep.", Print, WaitMode.Wait],
+    [tc.SetBootSlots(103, 0b111000, 0b111), Send, WaitMode.Wait],
+
+    # Power cycle EPS B
+    [tc.PowerCycleTelecommand(3), Send, WaitMode.Wait],
+
+    # Get beacons from deep-sleep
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
 ]
