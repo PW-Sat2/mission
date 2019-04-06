f1ad7245b55fa6ecddb1ccfbc40ec4a0686d8a68
Michał Gumiela <michal.gumiela@gmail.com>
2019-04-06 11:43:19 +0200

    808 - summary

---
+++ sessions/808/artifacts/tasklist.missing.py
@@ -0,0 +1,4 @@
+tasks = [
+[tc.DownloadFile(30, '/p1_128_2', [28, 29, 30, 31, 32, 33, 34]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/p1_128_1', [28, 29, 30, 31, 32, 33, 34]), Send, WaitMode.Wait]
+]
