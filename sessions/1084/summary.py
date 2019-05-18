331beea0db4cb016c5f4b559dbd910c7c20f70f1
begreeen <michal.gumiela@gmail.com>
2019-05-18 13:58:15 +0200

    session 1084

---
+++ sessions/1084/summary.py
@@ -0,0 +1,14 @@
+PHOTO_SESSIONS = [1082, 1083]
+
+lr_photos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+hr_photos = [1, 2, 3]
+
+
+for p in lr_photos:
+    extract_file('p{}_128_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_128_0'.format(p), output_file_name='assembled/p{}_128_0.jpg'.format(p))
+
+
+for p in hr_photos:
+    extract_file('p{}_480_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_480_0'.format(p), output_file_name='assembled/p{}_480_0.jpg'.format(p))
