68c411018e9b3fab5c197bb91e4a5c4a05f78f7c
Michał Gumiela <michal.gumiela@gmail.com>
2019-05-18 13:36:23 +0200

    1083 - summary

---
+++ sessions/1083/artifacts/tasklist.missing.py
@@ -0,0 +1,9 @@
+tasks = [
+[tc.DownloadFile(30, '/p8_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/p8_128_0', [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]), Send, WaitMode.Wait],
+	[tc.DownloadFile(32, '/telemetry.current', [197, 209, 234, 271]), Send, WaitMode.Wait],
+	[tc.DownloadFile(33, '/p9_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]), Send, WaitMode.Wait],
+	[tc.DownloadFile(34, '/p9_128_0', [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]), Send, WaitMode.Wait],
+	[tc.DownloadFile(35, '/p10_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]), Send, WaitMode.Wait],
+	[tc.DownloadFile(36, '/p10_128_0', [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]), Send, WaitMode.Wait]
+]
