4b0caaea20749f1fe9f5b694b4f9e55940405a8f
Michał Gumiela <michal.gumiela@gmail.com>
2019-05-19 10:29:07 +0200

    1087 - summary

---
+++ sessions/1087/artifacts/tasklist.missing.py
@@ -0,0 +1,14 @@
+tasks = [
+[tc.DownloadFile(30, '/telemetry.previous', [1382, 1389, 1395, 1401, 1407, 1413, 1419, 1425, 1432, 1439, 1445, 1451, 1457, 1463, 1469, 1475, 1482, 1489]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/telemetry.previous', [1495, 1501, 1507, 1513, 1519, 1525, 1532, 1539, 1545, 1551, 1557, 1563, 1569, 1575, 1582, 1589, 1595, 1601]), Send, WaitMode.Wait],
+	[tc.DownloadFile(32, '/telemetry.previous', [1607, 1613, 1619, 1625, 1632, 1639, 1645, 1651, 1657, 1663, 1669, 1675, 1682, 1689, 1695, 1701, 1707, 1713]), Send, WaitMode.Wait],
+	[tc.DownloadFile(33, '/telemetry.previous', [1719, 1725, 1732, 1739, 1745, 1751, 1757, 1763, 1769, 1775, 1782, 1789, 1795, 1801, 1807, 1813, 1819, 1825]), Send, WaitMode.Wait],
+	[tc.DownloadFile(34, '/telemetry.previous', [1832, 1839, 1845, 1851, 1857, 1863, 1869, 1875, 1882, 1889, 1895, 1901, 1907, 1913, 1919, 1925, 1932, 1939]), Send, WaitMode.Wait],
+	[tc.DownloadFile(35, '/telemetry.previous', [1945, 1951, 1957, 1963, 1969, 1975, 1982, 1989, 1995, 2001, 2007, 2013, 2019, 2025, 2032, 2039, 2045, 2051]), Send, WaitMode.Wait],
+	[tc.DownloadFile(36, '/telemetry.previous', [2057, 2063, 2069, 2075, 2082, 2089, 2095, 2101, 2107, 2113, 2119, 2125, 2132, 2139, 2145, 2151, 2157, 2163]), Send, WaitMode.Wait],
+	[tc.DownloadFile(37, '/telemetry.previous', [2169, 2175, 2182, 2189, 2195, 2201, 2207, 2213, 2219, 2225, 2232, 2239, 2245, 2251, 2257, 2263, 2269, 2275]), Send, WaitMode.Wait],
+	[tc.DownloadFile(38, '/telemetry.current', [2, 9, 15, 21, 27, 33, 39, 45, 52, 59, 65, 71, 77, 83, 89, 95, 102]), Send, WaitMode.Wait],
+	[tc.DownloadFile(39, '/telemetry.current', [109, 115, 121, 127, 133, 139, 145, 152, 159, 165, 171, 177, 183, 189, 195, 202, 209]), Send, WaitMode.Wait],
+	[tc.DownloadFile(40, '/telemetry.current', [215, 221, 227, 233, 239, 245, 252, 259, 265, 271, 277, 283, 289, 295, 302, 309, 315]), Send, WaitMode.Wait],
+	[tc.DownloadFile(41, '/telemetry.current', [321, 327, 333, 339, 345, 352, 359, 365, 371, 377, 383, 389, 395, 402, 409, 415]), Send, WaitMode.Wait]
+]
