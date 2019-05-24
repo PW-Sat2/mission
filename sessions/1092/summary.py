fddb571788c521dd3b34945776064e0801f706ae
begreeen <michal.gumiela@gmail.com>
2019-05-24 21:55:41 +0200

    fix to summary file

--- sessions/1092/summary.py
+++ sessions/1092/summary.py
@@ -1,2 +1,2 @@
-extract_file('suns_ps_5', also=[1088, 1089, 1090, 1091])
-parse_experiment('assembled/suns_ps_5')
+extract_file('suns_ps_4', also=[1088, 1089, 1090, 1091])
+parse_experiment('assembled/suns_ps_4')
