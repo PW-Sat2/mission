9825724b42d24113eb928f9b89aa2fa6ec0eb020
begreeen <michal.gumiela@gmail.com>
2019-06-01 12:35:19 +0200

    summary

--- sessions/1179/tasklist.py
+++ sessions/1179/tasklist.py
@@ -30,6 +30,13 @@ tasks = [
     [tc.DownloadFile(106, '/p8_128_0', [i for i in range(0, 35, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(107, '/p9_128_0', [i for i in range(0, 35, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(108, '/p10_128_0', [i for i in range(0, 35, 1)]), Send, WaitMode.Wait],
+
+    [tc.DownloadFile(45, '/p1_480_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]), Send, WaitMode.Wait],
+    [tc.DownloadFile(46, '/p1_480_0', [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]), Send, WaitMode.Wait],
+    [tc.DownloadFile(47, '/p1_480_0', [36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53]), Send, WaitMode.Wait],
+    [tc.DownloadFile(48, '/p1_480_0', [54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71]), Send, WaitMode.Wait],
+    [tc.DownloadFile(48, '/p1_480_0', [72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93]), Send, WaitMode.Wait],
+
     # file download end
 
 
