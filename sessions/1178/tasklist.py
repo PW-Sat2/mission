a716f03e01b26255cbd956d92fbbc45119c902cc
begreeen <michal.gumiela@gmail.com>
2019-06-01 09:18:17 +0200

    fix tasklist

--- sessions/1178/tasklist.py
+++ sessions/1178/tasklist.py
@@ -1,5 +1,5 @@
 tasks = [
-    # Generated on 2019-06-01 01:11:34.288000, contains telemetry from sessions 905 to 1060.
+    # Generated on 2019-06-01 01:11:34.288000.
     # The session starts on 2019-05-14 20:58:06+02:00.
 
 
@@ -12,15 +12,7 @@ tasks = [
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
     # auto-generated telemetry start
-    [tc.DownloadFile(30, '/telemetry.current', [5, 55, 105, 155, 205, 30, 80, 130, 180, 18, 68, 118, 168, 218, 42, 92, 142, 192, 12, 62]), Send, WaitMode.Wait],
-    [tc.DownloadFile(31, '/telemetry.current', [112, 162, 212, 24, 74, 124, 174, 224, 36, 86, 136, 186, 48, 98, 148, 198]), Send, WaitMode.Wait],
     # auto-generated telemetry end
-
-
-    # RadFET download
-    [tc.DownloadFile(40, '/radfet_13', range(0, 8)), Send, WaitMode.Wait],
-    [tc.DownloadFile(41, '/radfet_13', range(8, 16)), Send, WaitMode.Wait],
-
 
     ["The next step is Take Photo.", Print, WaitMode.Wait],
 
