572e55d8f80b2dc0b81ca656b6b35d1a044dd5eb
Maciej Nowak <maciejt.nowak@gmail.com>
2019-06-06 21:53:23 +0200

    Session 1216

---
+++ sessions/1216/tasklist.py
@@ -0,0 +1,19 @@
+tasks = [
+    # Set bitrate
+    [tc.SetBitrate(1, BaudRate.BaudRate9600), Send, WaitMode.Wait],
+
+    # Get few beacons from nominal OBC
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    ["Set bootslots for deep_sleep.", Print, WaitMode.NoWait],
+    [tc.SetBootSlots(103, 0b111000, 0b111), Send, WaitMode.Wait],
+
+    # Wait for good uplink before power cycle
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    # Power cycle EPS B
+    [tc.PowerCycleTelecommand(3), Send, WaitMode.Wait],
+
+    # Get beacons from deep-sleep
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
+]
