fb6aed708713dac2f8e3f5dc4e8999c51e0569e7
Grzegorz Gajoch <gajoch@gmail.com>
2019-06-15 18:46:36 +0000

    Session 1278

---
+++ sessions/1278/summary.py
@@ -0,0 +1,14 @@
+PHOTO_SESSIONS = [1275, 1277]
+
+photos_lr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+photos_hr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+for p in photos_lr:
+    extract_file('p{}_128_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_128_0'.format(p), output_file_name='assembled/p{}_128_0.jpg'.format(p))
+
+
+for p in photos_hr:
+    extract_file('p{}_480_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_480_0'.format(p), output_file_name='assembled/p{}_480_0.jpg'.format(p))
