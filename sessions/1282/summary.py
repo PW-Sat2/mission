252aa24ef9588f57ea4b7156723bd763aa235c9d
begreeen <michal.gumiela@gmail.com>
2019-06-16 13:36:34 +0200

    Revert "summary update"
    This reverts commit a03080b82b66a73193b4302d720ba6b1e9b566d4.

--- sessions/1282/summary.py
+++ sessions/1282/summary.py
@@ -1,7 +1,18 @@
-PHOTO_SESSIONS = [1279, 1281, 1282]
+PHOTO_SESSIONS = [1275, 1277, 1278, 1279, 1281]
 
+photos_lr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 photos_hr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+for p in photos_lr:
+    extract_file('p{}_128_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_128_0'.format(p), output_file_name='assembled/p{}_128_0.jpg'.format(p))
+
 
 for p in photos_hr:
     extract_file('p{}_480_0'.format(p), also=PHOTO_SESSIONS)
     decode_photo('assembled/p{}_480_0'.format(p), output_file_name='assembled/p{}_480_0.jpg'.format(p))
+
+extract_file('suns_ps_7', also=[1281])
+
+parse_experiment('assembled/suns_ps_7')
