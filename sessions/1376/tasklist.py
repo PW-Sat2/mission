7ec5676728b9dacc2722aad57dba57d594c47130
Piotr Kuligowski <xsaperx@gmail.com>
2019-06-29 21:07:32 +0200

    chunks correction

--- sessions/1376/tasklist.py
+++ sessions/1376/tasklist.py
@@ -72,13 +72,14 @@ tasks = [
     [tc.DownloadFile(151, '/p8_240_0', [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]), Send, WaitMode.Wait],
     [tc.DownloadFile(152, '/p8_240_0', [36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53]), Send, WaitMode.Wait],
     [tc.DownloadFile(153, '/p8_240_0', [54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71]), Send, WaitMode.Wait],
-    [tc.DownloadFile(154, '/p8_240_0', [72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]), Send, WaitMode.Wait],
+    [tc.DownloadFile(154, '/p8_240_0', [72, 73, 74, 75, 76, 77]), Send, WaitMode.Wait],
 
     [tc.DownloadFile(160, '/p9_240_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]), Send, WaitMode.Wait],
     [tc.DownloadFile(161, '/p9_240_0', [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]), Send, WaitMode.Wait],
     [tc.DownloadFile(162, '/p9_240_0', [36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53]), Send, WaitMode.Wait],
     [tc.DownloadFile(163, '/p9_240_0', [54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71]), Send, WaitMode.Wait],
-    [tc.DownloadFile(164, '/p9_240_0', [72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]), Send, WaitMode.Wait],
+    [tc.DownloadFile(164, '/p9_240_0', [72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89]), Send, WaitMode.Wait],
+    [tc.DownloadFile(165, '/p9_240_0', [90, 91, 92, 93, 94, 95, 96, 97, 98]), Send, WaitMode.Wait],
 
     [tc.DownloadFile(170, '/p10_128_0', [i for i in range(0, 20, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(171, '/p10_128_0', [i for i in range(20, 40, 1)]), Send, WaitMode.Wait],
@@ -86,8 +87,7 @@ tasks = [
     [tc.DownloadFile(180, '/p10_240_0', [i for i in range(0, 20, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(181, '/p10_240_0', [i for i in range(20, 40, 1)]), Send, WaitMode.Wait],
     [tc.DownloadFile(182, '/p10_240_0', [i for i in range(40, 60, 1)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(183, '/p10_240_0', [i for i in range(60, 80, 1)]), Send, WaitMode.Wait],
-    [tc.DownloadFile(184, '/p10_240_0', [i for i in range(80, 90, 1)]), Send, WaitMode.Wait],
+    [tc.DownloadFile(183, '/p10_240_0', [i for i in range(60, 74, 1)]), Send, WaitMode.Wait],
 
     [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
 ]
