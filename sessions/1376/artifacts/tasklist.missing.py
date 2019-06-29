eca62fe4a4ba303214aa9197c2f4f06ab5b1adac
Michał Gumiela <michal.gumiela@gmail.com>
2019-06-29 22:55:07 +0200

    1376 - summary

--- sessions/1376/artifacts/tasklist.missing.py
+++ sessions/1376/artifacts/tasklist.missing.py
@@ -1,17 +1,4 @@
 tasks = [
-[tc.DownloadFile(30, '/telemetry.current', [366, 894, 990]), Send, WaitMode.Wait],
-	[tc.DownloadFile(31, '/p10_240_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]), Send, WaitMode.Wait],
-	[tc.DownloadFile(32, '/p10_240_0', [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37]), Send, WaitMode.Wait],
-	[tc.DownloadFile(33, '/p10_240_0', [38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55]), Send, WaitMode.Wait],
-	[tc.DownloadFile(34, '/p10_240_0', [56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73]), Send, WaitMode.Wait],
-	[tc.DownloadFile(35, '/p8_240_0', [16, 18, 19, 21, 24, 26, 27, 29, 31, 65, 66, 67, 71, 72, 73, 74, 75, 76, 77]), Send, WaitMode.Wait],
-	[tc.DownloadFile(36, '/p9_240_0', [4, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]), Send, WaitMode.Wait],
-	[tc.DownloadFile(37, '/p9_240_0', [26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44]), Send, WaitMode.Wait],
-	[tc.DownloadFile(38, '/p9_240_0', [45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62]), Send, WaitMode.Wait],
-	[tc.DownloadFile(39, '/p9_240_0', [63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80]), Send, WaitMode.Wait],
-	[tc.DownloadFile(40, '/p9_240_0', [81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98]), Send, WaitMode.Wait],
-	[tc.DownloadFile(41, '/p10_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]), Send, WaitMode.Wait],
-	[tc.DownloadFile(42, '/p10_128_0', [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]), Send, WaitMode.Wait],
-	[tc.DownloadFile(43, '/p10_128_0', [27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39]), Send, WaitMode.Wait],
-	[tc.DownloadFile(44, '/p2_480_0', [36, 64, 94]), Send, WaitMode.Wait]
+[tc.DownloadFile(30, '/p10_240_0', [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/telemetry.current', [366, 894, 990]), Send, WaitMode.Wait]
 ]
