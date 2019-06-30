58d17509e4fe0ed842be824aa12a0060a948ae30
begreeen <michal.gumiela@gmail.com>
2019-06-30 11:57:38 +0200

    session 1380

---
+++ sessions/1380/summary.py
@@ -0,0 +1,16 @@
+PHOTO_SESSIONS = [1372, 1373, 1376, 1377, 1378, 1379]
+
+photos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for p in photos:
+    extract_file('p{}_128_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_128_0'.format(p), output_file_name='assembled/p{}_128_0.jpg'.format(p))
+
+    extract_file('p{}_240_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_240_0'.format(p), output_file_name='assembled/p{}_240_0.jpg'.format(p))
+
+    extract_file('p{}_480_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_480_0'.format(p), output_file_name='assembled/p{}_480_0.jpg'.format(p))
+
+
+extract_file('suns_ps_7', also=[1379])
