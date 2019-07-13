6110872026340708b3bf6d2bc224181eb66f981d
begreeen <michal.gumiela@gmail.com>
2019-07-14 00:31:24 +0200

    session-1465

---
+++ sessions/1465/summary.py
@@ -0,0 +1,16 @@
+PHOTO_SESSIONS = [1460, 1461, 1462, 1463, 1464]
+
+photos_queue = range(1, 51)
+photos_series = range(0, 29)
+
+for p in photos_queue:
+    extract_file('p{}_128_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_128_0'.format(p), output_file_name='assembled/p{}_128_0.jpg'.format(p))
+
+for p in photos_series:
+    extract_file('p52_128_{}'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p52_128_{}'.format(p), output_file_name='assembled/p52_128_{}.jpg'.format(p))
+
+for p in ['p0n_480_0', 'p0w_480_0']:
+    extract_file(p, also=PHOTO_SESSIONS)
+    decode_photo(p, output_file_name='assembled/{}.jpg'.format(p))
