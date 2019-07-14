e731a436e235a504189e361db11371bbcfb27695
Michał Gumiela <michal.gumiela@gmail.com>
2019-07-14 11:48:43 +0200

    1466 - summary

---
+++ sessions/1466/artifacts/tasklist.missing.py
@@ -0,0 +1,22 @@
+tasks = [
+[tc.DownloadFile(30, '/p52_128_16', [16, 17, 19, 21, 22, 23, 25, 29, 30, 31, 35]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/telemetry.previous', [2025, 2032, 2039, 2057, 2063, 2069, 2101, 2107, 2119, 2125, 2145, 2163, 2169, 2175, 2182]), Send, WaitMode.Wait],
+	[tc.DownloadFile(32, '/telemetry.previous', [2189, 2195, 2201, 2207, 2213, 2219, 2225, 2232, 2245, 2251, 2257, 2263, 2269, 2275]), Send, WaitMode.Wait],
+	[tc.DownloadFile(33, '/p9_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]), Send, WaitMode.Wait],
+	[tc.DownloadFile(34, '/p24_128_0', [20, 22]), Send, WaitMode.Wait],
+	[tc.DownloadFile(35, '/suns_ps_9', [7, 10, 11, 12, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]), Send, WaitMode.Wait],
+	[tc.DownloadFile(36, '/suns_ps_9', [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]), Send, WaitMode.Wait],
+	[tc.DownloadFile(37, '/suns_ps_9', [51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70]), Send, WaitMode.Wait],
+	[tc.DownloadFile(38, '/suns_ps_9', [71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]), Send, WaitMode.Wait],
+	[tc.DownloadFile(39, '/suns_ps_9', [91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110]), Send, WaitMode.Wait],
+	[tc.DownloadFile(40, '/suns_ps_9', [111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130]), Send, WaitMode.Wait],
+	[tc.DownloadFile(41, '/suns_ps_9', [131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150]), Send, WaitMode.Wait],
+	[tc.DownloadFile(42, '/suns_ps_9', [151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170]), Send, WaitMode.Wait],
+	[tc.DownloadFile(43, '/suns_ps_9', [171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190]), Send, WaitMode.Wait],
+	[tc.DownloadFile(44, '/suns_ps_9', [191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210]), Send, WaitMode.Wait],
+	[tc.DownloadFile(45, '/suns_ps_9', [211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230]), Send, WaitMode.Wait],
+	[tc.DownloadFile(46, '/suns_ps_9', [231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249]), Send, WaitMode.Wait],
+	[tc.DownloadFile(47, '/p41_128_0', [4, 5]), Send, WaitMode.Wait],
+	[tc.DownloadFile(48, '/p20_128_0', [25]), Send, WaitMode.Wait],
+	[tc.DownloadFile(49, '/p27_128_0', [0, 3, 4, 6, 7, 8]), Send, WaitMode.Wait]
+]
