adc97a1c224fbc8c0b7bab2610538f5e946ffe5a
operator <sat_operator@future-processing.com>
2019-07-14 21:03:58 +0200

    1468 - summary

---
+++ sessions/1468/artifacts/tasklist.missing.py
@@ -0,0 +1,4 @@
+tasks = [
+[tc.DownloadFile(30, '/telemetry.current', [571, 583, 621, 633, 671, 709, 721, 733, 771, 821, 833, 871, 883, 909, 921, 933, 971, 997, 1009]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/telemetry.current', [1033, 1047, 1059, 1077, 1083, 1090, 1109, 1121, 1133, 1147, 1183, 1221, 1233, 1271, 1309, 1359, 1371, 1377]), Send, WaitMode.Wait]
+]
