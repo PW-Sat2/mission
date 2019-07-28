1d462dd523b2dddf4de7414108c93ff2d904a9e9
Michał Gumiela <michal.gumiela@gmail.com>
2019-07-28 23:09:25 +0200

    1553 - summary

--- sessions/1553/artifacts/tasklist.missing.py
+++ sessions/1553/artifacts/tasklist.missing.py
@@ -1,5 +1,5 @@
 tasks = [
 [tc.DownloadFile(30, '/p1_480_0', [0]), Send, WaitMode.Wait],
 	[tc.DownloadFile(31, '/p_480_11', [57]), Send, WaitMode.Wait],
-	[tc.DownloadFile(32, '/p_480_10', [4, 6, 9, 98]), Send, WaitMode.Wait]
+	[tc.DownloadFile(32, '/p_480_10', [4, 6, 9]), Send, WaitMode.Wait]
 ]
