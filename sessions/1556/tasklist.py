9f540d3d4f99e46c1d578795e4f327cc2ac60417
Michał Gumiela <michal.gumiela@gmail.com>
2019-07-29 10:25:56 +0200

    Update tasklist.py

--- sessions/1556/tasklist.py
+++ sessions/1556/tasklist.py
@@ -2,14 +2,17 @@ tasks = [
     # Generated on 2019-07-29 08:52:01.901000, contains telemetry from sessions 1553 to 1555.
     # The session starts on 2019-07-29 10:07:09+02:00.
 
-    [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
-
     [[tc.SendBeacon(), 10], SendLoop, WaitMode.NoWait],
 
+    # buffor to not wake up the sat accidentally 
     [tc.SendBeacon(), Send, WaitMode.Wait],
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+    
+    # Get beacons from deep-sleep
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
 
-
-
+    # only in case it's not already in the deep sleep
     ["Set bootslots for deep_sleep.", Print, WaitMode.NoWait],
     [tc.SetBootSlots(103, 0b111000, 0b111), Send, WaitMode.Wait],
 
