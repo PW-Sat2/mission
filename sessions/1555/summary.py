3ec33a4ef62ef03d55bd1fe6335b6bc2ee617bcf
Michał Gumiela <michal.gumiela@gmail.com>
2019-07-29 09:35:39 +0200

    summary

---
+++ sessions/1555/summary.py
@@ -0,0 +1,18 @@
+PHOTO_SESSIONS = [1544, 1546, 1547, 1549, 1550, 1551, 1552, 1553]
+
+photos = range(0, 29)
+
+photos_single_low_res = range(1, 7)
+photos_single_high_res = [1, 2]
+
+for p in photos:
+    extract_file('p_480_{}'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p_480_{}'.format(p), output_file_name='assembled/p_480_{}.jpg'.format(p))
+
+for p in photos_single_low_res:
+    extract_file('p{}_128_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_128_0'.format(p), output_file_name='assembled/p{}_128_0.jpg'.format(p))
+
+for p in photos_single_high_res:
+    extract_file('p{}_480_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_480_0'.format(p), output_file_name='assembled/p{}_480_0.jpg'.format(p))
