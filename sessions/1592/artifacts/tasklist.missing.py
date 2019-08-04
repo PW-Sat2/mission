57d51fc5d615dd3fc8ff3a0e7fb41e38940b91e6
operator <sat_operator@future-processing.com>
2019-08-04 12:16:31 +0200

    1592 - summary

--- sessions/1592/artifacts/tasklist.missing.py
+++ sessions/1592/artifacts/tasklist.missing.py
@@ -1,6 +1,3 @@
 tasks = [
-[tc.DownloadFile(30, '/suns_ps_10_sec', [663, 758, 763, 771, 772, 773, 776, 777, 778, 779, 780, 781, 782, 783, 784, 785]), Send, WaitMode.Wait],
-	[tc.DownloadFile(31, '/suns_ps_10_sec', [786, 787, 788, 789, 790, 791, 792, 793, 794, 795, 796, 797, 798, 799, 800, 801]), Send, WaitMode.Wait],
-	[tc.DownloadFile(32, '/suns_ps_10_sec', [802, 803, 804, 805, 806, 807, 808, 809, 810, 811, 812, 813, 814, 815, 816, 817]), Send, WaitMode.Wait],
-	[tc.DownloadFile(33, '/suns_ps_10_sec', [818, 819, 820, 821, 822, 823, 824, 825, 826, 827, 828, 829, 830, 831, 832, 833]), Send, WaitMode.Wait]
+[tc.DownloadFile(30, '/suns_ps_10_sec', [663]), Send, WaitMode.Wait]
 ]
