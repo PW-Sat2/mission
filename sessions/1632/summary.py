9cce228539e31225783d4ae652b8ec86b643ff6d
begreeen <michal.gumiela@gmail.com>
2019-08-11 00:48:54 +0200

    summary file for 1632

---
+++ sessions/1632/summary.py
@@ -0,0 +1,13 @@
+PHOTO_SESSIONS = [1630, 1631, 1632]
+
+photos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+photos_hr = [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+
+for p in photos:
+    extract_file('p{}_128_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_128_0'.format(p), output_file_name='assembled/p{}_128_0.jpg'.format(p))
+
+for p in photos_hr:
+    extract_file('p{}_480_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_480_0'.format(p), output_file_name='assembled/p{}_480_0.jpg'.format(p))
