dbc8b59df71404e807692baed5a76f6e71400d03
Piotr Kuligowski <xsaperx@gmail.com>
2019-08-24 09:43:37 +0200

    take high-res photo over PL

--- sessions/1711/tasklist.py
+++ sessions/1711/tasklist.py
@@ -12,8 +12,16 @@ tasks = [
     # auto-generated telemetry start
     # auto-generated telemetry end
 
-    # 19th RadFET Experiment
+    # 20th RadFET Experiment
     [tc.PerformRadFETExperiment(10, 150, 110, 'radfet_20'), Send, WaitMode.Wait],
+    
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    [tc.TakePhotoTelecommand(11, CameraLocation.Wing, PhotoResolution.p128, 1, datetime.timedelta(minutes=0), 'x1_128'), Send, WaitMode.Wait],
+    [tc.TakePhotoTelecommand(12, CameraLocation.Wing, PhotoResolution.p480, 1, datetime.timedelta(minutes=0), 'x1_480'), Send, WaitMode.Wait],
+
+    [tc.TakePhotoTelecommand(13, CameraLocation.Nadir, PhotoResolution.p128, 1, datetime.timedelta(minutes=1), 'x2_128'), Send, WaitMode.Wait],
+    [tc.TakePhotoTelecommand(14, CameraLocation.Nadir, PhotoResolution.p480, 1, datetime.timedelta(minutes=0), 'x2_480'), Send, WaitMode.Wait],
 
     [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
 ]
