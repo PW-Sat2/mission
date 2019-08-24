235b3d4816217499e2721e224a659f1dbe57e087
Tomasz Martyniak <tom4r@users.noreply.github.com>
2019-08-24 22:02:46 +0200

    Summary and past successes.

--- sessions/1715/summary.py
+++ sessions/1715/summary.py
@@ -1,5 +1,8 @@
 PHOTO_SESSIONS = [1715]
 
+extract_file('suns_ps_11', also=[1714])
+extract_file('radfet_20', also=[1712, 1714])
+
 photos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 38, 39]
 
 for p in photos:
