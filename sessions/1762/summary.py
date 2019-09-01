2cd589a248ae5d54d4ed6d228793e272ee0ca554
begreeen <michal.gumiela@gmail.com>
2019-09-01 22:02:04 +0200

    summary file

---
+++ sessions/1762/summary.py
@@ -0,0 +1,8 @@
+extract_file('suns_ps_12_sec', also=[1754, 1755, 1756, 1757, 1758, 1759, 1760])
+extract_file('suns_ps_12', also=[1754, 1755, 1756])
+
+extract_file('suns_ps_13_sec', also=[1754, 1755, 1756, 1757, 1758, 1759, 1760])
+extract_file('suns_ps_13', also=[1754, 1755, 1756])
+
+extract_file('suns_ps_14_sec', also=[1754, 1755, 1756, 1757, 1758, 1759, 1760, 1761])
+extract_file('suns_ps_14', also=[1754, 1755, 1756, 1757, 1758, 1759, 1760, 1761, 1762])
