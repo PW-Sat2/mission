e0fb7bc179772c376443aafd14cbda41db749f99
Grzegorz Gajoch <gajoch@gmail.com>
2019-09-07 01:59:49 +0200

    Session 1795 (#557)
    * plannned fixed
    
    * Session 1795
    
    * fix

---
+++ sessions/1795/tasklist.py
@@ -0,0 +1,20 @@
+tasks = [
+    # Get beacons from deep-sleep
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
+
+    # Wake-up from deep-sleep
+    [tc.PingTelecommand(), Send, WaitMode.Wait],
+
+    # Set bitrate
+    [tc.SetBitrate(1, BaudRate.BaudRate9600), Send, WaitMode.Wait],
+
+    # Get beacon
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
+
+    # 21st RadFET Experiment
+    [tc.PerformRadFETExperiment(10, 150, 110, 'radfet_21'), Send, WaitMode.Wait],
+
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
+]
