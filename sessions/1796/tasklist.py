ab854f4c058b533ae5334cecc23c27e1c019ab35
Tomasz Martyniak <tom4r@users.noreply.github.com>
2019-09-07 10:30:13 +0200

    Update after 1795.

--- sessions/1796/tasklist.py
+++ sessions/1796/tasklist.py
@@ -1,19 +1,20 @@
 tasks = [
-     [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
+   # Get beacons from deep-sleep
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
 
-    [[tc.SendBeacon(), 10], SendLoop, WaitMode.NoWait],
+    # Wake-up from deep-sleep
+    [tc.PingTelecommand(), Send, WaitMode.Wait],
+
+    # Set bitrate
+    [tc.SetBitrate(1, BaudRate.BaudRate9600), Send, WaitMode.Wait],
+
+    # Get beacon
+    [tc.SendBeacon(), Send, WaitMode.Wait],
 
     [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
 
-    [tc.SendBeacon(), Send, WaitMode.Wait],
-
-    # auto-generated telemetry start
-    [tc.DownloadFile(30, '/telemetry.current', [1, 51, 101, 151, 201, 26, 76, 126, 176, 14, 64, 114, 164, 214, 38, 88, 138, 188, 8, 58]), Send, WaitMode.Wait],
-    [tc.DownloadFile(31, '/telemetry.current', [108, 158, 208, 20, 70, 120, 170, 220, 32, 82, 132, 182, 44, 94, 144, 194]), Send, WaitMode.Wait],
-    # auto-generated telemetry end
-
-    # missing, part 1
-    [tc.DownloadFile(200, '/radfet_21', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]), Send, WaitMode.Wait],
+    # 21st RadFET Experiment
+    [tc.PerformRadFETExperiment(10, 150, 110, 'radfet_21'), Send, WaitMode.Wait],
 
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
