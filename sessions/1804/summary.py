dc859ce113ee54f65007f45b08cd41ac0c243d28
begreeen <michal.gumiela@gmail.com>
2019-09-08 14:32:16 +0200

    session 1804

---
+++ sessions/1804/summary.py
@@ -0,0 +1,16 @@
+PHOTO_SESSIONS = [1798, 1799, 1800, 1801, 1802, 1803]
+
+photos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+for p in photos:
+    extract_file('t{:02}w_480_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/t{:02}w_480_0'.format(p), output_file_name='assembled/t{:02}w_480_0.jpg'.format(p))
+
+    extract_file('t{:02}n_480_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/t{:02}n_480_0'.format(p), output_file_name='assembled/t{:02}n_480_0.jpg'.format(p))
+
+photosDummy = [2, 3, 4, 5]
+
+for p in photosDummy:
+    extract_file('dummy_{}_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/dummy_{}_0'.format(p), output_file_name='assembled/dummy_{}_0.jpg'.format(p))
