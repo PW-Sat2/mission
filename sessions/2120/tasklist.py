7f97b6dab11fc918655c95dd556ca1747dfe45a9
Tomasz Martyniak <tom4r@users.noreply.github.com>
2019-10-27 22:45:12 +0100

    Session 2120.

---
+++ sessions/2120/tasklist.py
@@ -0,0 +1,41 @@
+tasks = [
+    # Generated on 2019-10-27 22:36:30.273000, contains telemetry from sessions 2118 to 2120.
+    # The session starts on 2019-10-27 23:56:42+01:00.
+
+    [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
+
+    [[tc.SendBeacon(), 10], SendLoop, WaitMode.NoWait],
+
+    [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
+
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    # missing from previous session start
+    [tc.DownloadFile(30, '/t02w_240_17', [40, 47]), Send, WaitMode.Wait],
+    [tc.DownloadFile(31, '/t02w_240_7', [12, 13, 14, 16, 17, 19, 23, 24, 25, 26, 33, 38, 39]), Send, WaitMode.Wait],
+    [tc.DownloadFile(32, '/t02w_240_7', [40, 41, 43, 46, 47, 48, 49, 50, 51, 53, 54, 55]), Send, WaitMode.Wait],
+    [tc.DownloadFile(33, '/t02w_240_9', [0, 2, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]), Send, WaitMode.Wait],
+    [tc.DownloadFile(34, '/t02w_240_9', [36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54]), Send, WaitMode.Wait],
+    [tc.DownloadFile(35, '/t02w_240_27', [1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]), Send, WaitMode.Wait],
+    [tc.DownloadFile(36, '/t02w_240_27', [22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39]), Send, WaitMode.Wait],
+    [tc.DownloadFile(37, '/t02w_240_27', [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57]), Send, WaitMode.Wait],
+    [tc.DownloadFile(38, '/t02w_240_27', [58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]), Send, WaitMode.Wait],
+    [tc.DownloadFile(39, '/t02w_240_28', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]), Send, WaitMode.Wait],
+    [tc.DownloadFile(40, '/t02w_240_28', [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]), Send, WaitMode.Wait],
+    [tc.DownloadFile(41, '/t02w_240_28', [33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48]), Send, WaitMode.Wait],
+    [tc.DownloadFile(42, '/t02w_240_28', [49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64]), Send, WaitMode.Wait],
+    # missing from previous session end
+
+
+    # auto-generated file download start
+    
+    # auto-generated file download end
+
+
+    # auto-generated file remove start
+    
+    # auto-generated file remove end
+
+
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
+]
