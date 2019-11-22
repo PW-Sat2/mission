b4062308bcc2512c6a6c016b48a92dc56c427c07
Tomasz Martyniak <tom4r@users.noreply.github.com>
2019-11-22 23:26:15 +0100

    Session 2289.

---
+++ sessions/2289/summary.py
@@ -0,0 +1,10 @@
+PHOTO_SESSIONS = [2288]
+
+lr_photos = range(0, 29)
+
+for p in lr_photos:
+    extract_file('t01w_240_{}'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/t01w_240_{}'.format(p), output_file_name='assembled/t01w_240_{}.jpg'.format(p))
+
+    extract_file('t02w_240_{}'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/t02w_240_{}'.format(p), output_file_name='assembled/t02w_240_{}.jpg'.format(p))
