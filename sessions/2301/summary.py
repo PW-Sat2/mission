e6c2df8dde21bb81ba42346b22abcb48d68bcc2b
Tomasz Martyniak <tom4r@users.noreply.github.com>
2019-11-24 21:27:48 +0100

    Summary fix.

--- sessions/2301/summary.py
+++ sessions/2301/summary.py
@@ -8,3 +8,9 @@ for p in lr_photos:
 
     extract_file('t02w_240_{}'.format(p), also=PHOTO_SESSIONS)
     decode_photo('assembled/t02w_240_{}'.format(p), output_file_name='assembled/t02w_240_{}.jpg'.format(p))
+
+    extract_file('t03n_240_{}'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/t03n_240_{}'.format(p), output_file_name='assembled/t03n_240_{}.jpg'.format(p))
+
+    extract_file('t04n_240_{}'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/t04n_240_{}'.format(p), output_file_name='assembled/t04n_240_{}.jpg'.format(p))
