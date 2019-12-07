01f284d6f6e364e198f8f30ad5e4e6de954b8454
Tomasz Martyniak <tom4r@users.noreply.github.com>
2019-12-08 00:03:09 +0100

    Session 2393.

---
+++ sessions/2393/summary.py
@@ -0,0 +1,9 @@
+extract_file('suns_ps_16', also=[2386, 2387, 2388, 2390, 2391])
+
+PHOTO_SESSIONS = [2391, 2392]
+
+lr_photos = range(0, 29)
+
+for p in lr_photos:
+    extract_file('t01w_240_{}'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/t01w_240_{}'.format(p), output_file_name='assembled/t01w_240_{}.jpg'.format(p))
