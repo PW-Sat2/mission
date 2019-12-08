8be42a32c074adb48c8cf982505777d49ca2dc92
begreeen <michal.gumiela@gmail.com>
2019-12-08 11:51:29 +0100

    session 2395

---
+++ sessions/2395/summary.py
@@ -0,0 +1,7 @@
+PHOTO_SESSIONS = [2391, 2392, 2393, 2394]
+
+lr_photos = range(0, 29)
+
+for p in lr_photos:
+    extract_file('t01w_240_{}'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/t01w_240_{}'.format(p), output_file_name='assembled/t01w_240_{}.jpg'.format(p))
