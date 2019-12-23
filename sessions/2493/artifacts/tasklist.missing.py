4760a8d8c991985894f7c88e4d38ac231cdaa092
Michał Gumiela <michal.gumiela@gmail.com>
2019-12-23 21:15:56 +0100

    2493 - summary

---
+++ sessions/2493/artifacts/tasklist.missing.py
@@ -0,0 +1,15 @@
+tasks = [
+[tc.DownloadFile(30, '/t04w_240_1', [18, 19, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/t04w_240_1', [35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46]), Send, WaitMode.Wait],
+	[tc.DownloadFile(32, '/t04w_240_2', [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]), Send, WaitMode.Wait],
+	[tc.DownloadFile(33, '/t04w_240_2', [28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]), Send, WaitMode.Wait],
+	[tc.DownloadFile(34, '/t03w_240_26', [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]), Send, WaitMode.Wait],
+	[tc.DownloadFile(35, '/t03w_240_26', [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46]), Send, WaitMode.Wait],
+	[tc.DownloadFile(36, '/t03w_240_26', [47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61]), Send, WaitMode.Wait],
+	[tc.DownloadFile(37, '/t03w_240_27', [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]), Send, WaitMode.Wait],
+	[tc.DownloadFile(38, '/t03w_240_27', [34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49]), Send, WaitMode.Wait],
+	[tc.DownloadFile(39, '/t03w_240_27', [50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65]), Send, WaitMode.Wait],
+	[tc.DownloadFile(40, '/t03w_240_28', [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]), Send, WaitMode.Wait],
+	[tc.DownloadFile(41, '/t03w_240_28', [36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53]), Send, WaitMode.Wait],
+	[tc.DownloadFile(42, '/t03w_240_28', [54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71]), Send, WaitMode.Wait]
+]
