7f5ed97b1d0df6f639178a8a82573a5bd663d740
Michał Gumiela <michal.gumiela@gmail.com>
2020-01-19 11:37:27 +0100

    2667 - summary

---
+++ sessions/2667/artifacts/tasklist.missing.py
@@ -0,0 +1,12 @@
+tasks = [
+[tc.DownloadFile(30, '/p07w_480_0', [50, 53, 54, 72, 108, 109, 111, 123, 128, 129, 131]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/p08n_480_0', [23, 40, 45, 50, 78]), Send, WaitMode.Wait],
+	[tc.DownloadFile(32, '/p09n_480_0', [25, 26, 31, 94]), Send, WaitMode.Wait],
+	[tc.DownloadFile(33, '/p09w_480_0', [43, 44, 45, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69]), Send, WaitMode.Wait],
+	[tc.DownloadFile(34, '/p09w_480_0', [70, 71, 72, 73, 74, 75, 76, 77, 80, 82, 124, 125, 126]), Send, WaitMode.Wait],
+	[tc.DownloadFile(35, '/p08w_480_0', [34, 35, 36, 37, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55]), Send, WaitMode.Wait],
+	[tc.DownloadFile(36, '/p08w_480_0', [56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74]), Send, WaitMode.Wait],
+	[tc.DownloadFile(37, '/p08w_480_0', [75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93]), Send, WaitMode.Wait],
+	[tc.DownloadFile(38, '/p08w_480_0', [94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112]), Send, WaitMode.Wait],
+	[tc.DownloadFile(39, '/p08w_480_0', [113, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169]), Send, WaitMode.Wait]
+]
