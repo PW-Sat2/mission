689cdcb1cb74b5d66a92d046a1acd8a21da55f7a
Tomasz Martyniak <tom4r@users.noreply.github.com>
2020-01-31 20:20:22 +0100

    Session 2748.

---
+++ sessions/2748/tasklist.py
@@ -0,0 +1,30 @@
+tasks = [
+    # Get beacons from deep-sleep
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
+
+    # Wake-up from deep-sleep
+    [tc.PingTelecommand(), Send, WaitMode.Wait],
+
+    # Set bitrate
+    [tc.SetBitrate(1, BaudRate.BaudRate9600), Send, WaitMode.Wait],
+
+    # Get beacon
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
+
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+    
+    [tc.PerformRadFETExperiment(10, 150, 110, 'radfet_31'), Send, WaitMode.Wait],
+
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+     # Long Movie Strikes Back
+    [tc.TakePhotoTelecommand(100, CameraLocation.Wing, PhotoResolution.p240, 29, datetime.timedelta(minutes=1), 't01w_240'), Send, WaitMode.Wait],
+    [tc.TakePhotoTelecommand(101, CameraLocation.Wing, PhotoResolution.p240, 29, datetime.timedelta(seconds=0), 't02w_240'), Send, WaitMode.Wait],
+    [tc.TakePhotoTelecommand(102, CameraLocation.Wing, PhotoResolution.p240, 29, datetime.timedelta(seconds=0), 't03w_240'), Send, WaitMode.Wait],
+    [tc.TakePhotoTelecommand(103, CameraLocation.Wing, PhotoResolution.p240, 7, datetime.timedelta(seconds=0), 't04w_240'), Send, WaitMode.Wait],
+
+    # Waste session time
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
+]
