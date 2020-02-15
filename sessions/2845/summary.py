7356983b3ebaaf0f341c6a4f94f9fb429c442c18
Marcin Drobik <mdrobik@kplabs.pl>
2020-02-15 21:42:12 +0100

    Fixed error in summry.py session 2845

--- sessions/2845/summary.py
+++ sessions/2845/summary.py
@@ -2,6 +2,6 @@ PHOTO_SESSIONS = [2844]
 
 photos = range(0, 9)
 
-for p in lr_photos:
+for p in photos:
     extract_file('p{}_480_0'.format(p), also=PHOTO_SESSIONS)
     decode_photo('assembled/p{}_480_0'.format(p), output_file_name='assembled/p{}_480_0.jpg'.format(p))
