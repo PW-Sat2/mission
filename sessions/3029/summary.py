862c651168562e40d823e348b53c640b5ba5e874
begreeen <michal.gumiela@gmail.com>
2020-03-14 20:45:05 +0100

    session-3029

---
+++ sessions/3029/summary.py
@@ -0,0 +1,10 @@
+PHOTO_SESSIONS = [3024, 3025, 3026, 3027, 3028, 3029]
+
+photos = range(1, 11)
+
+for p in photos:
+    extract_file('p{}_128_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_128_0'.format(p), output_file_name='assembled/p{}_128_0.jpg'.format(p))
+
+    extract_file('p{}_480_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_480_0'.format(p), output_file_name='assembled/p{}_480_0.jpg'.format(p))
