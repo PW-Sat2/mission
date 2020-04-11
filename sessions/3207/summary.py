fd74da6660ae0f1265702c328a418b1dd96ed232
begreeen <michal.gumiela@gmail.com>
2020-04-11 11:58:00 +0200

    add tasklist entries for downloaded photo

---
+++ sessions/3207/summary.py
@@ -0,0 +1,8 @@
+PHOTO_SESSIONS = []
+
+for p in range(1, 11):
+    extract_file('p{}_128_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_128_0'.format(p), output_file_name='assembled/p{}_128_0.jpg'.format(p))
+
+    extract_file('p{}_480_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_480_0'.format(p), output_file_name='assembled/p{}_480_0.jpg'.format(p))
