31c61ea78004a107dea46e9a0cb1994a45219f08
Michał Gumiela <michal.gumiela@gmail.com>
2020-04-11 13:56:01 +0200

    3208 - summary

--- sessions/3208/artifacts/tasklist.missing.py
+++ sessions/3208/artifacts/tasklist.missing.py
@@ -1,27 +1,4 @@
 tasks = [
 [tc.DownloadFile(30, '/telemetry.current', [162]), Send, WaitMode.Wait],
-	[tc.DownloadFile(31, '/p1_128_0', [2, 3, 4, 5, 6, 14, 23, 25]), Send, WaitMode.Wait],
-	[tc.DownloadFile(32, '/radfet_36', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]), Send, WaitMode.Wait],
-	[tc.DownloadFile(33, '/p10_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]), Send, WaitMode.Wait],
-	[tc.DownloadFile(34, '/p10_128_0', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]), Send, WaitMode.Wait],
-	[tc.DownloadFile(35, '/p2_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]), Send, WaitMode.Wait],
-	[tc.DownloadFile(36, '/p2_128_0', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]), Send, WaitMode.Wait],
-	[tc.DownloadFile(37, '/p3_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]), Send, WaitMode.Wait],
-	[tc.DownloadFile(38, '/p3_128_0', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]), Send, WaitMode.Wait],
-	[tc.DownloadFile(39, '/p4_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]), Send, WaitMode.Wait],
-	[tc.DownloadFile(40, '/p4_128_0', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]), Send, WaitMode.Wait],
-	[tc.DownloadFile(41, '/p5_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]), Send, WaitMode.Wait],
-	[tc.DownloadFile(42, '/p5_128_0', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]), Send, WaitMode.Wait],
-	[tc.DownloadFile(43, '/p6_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]), Send, WaitMode.Wait],
-	[tc.DownloadFile(44, '/p6_128_0', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]), Send, WaitMode.Wait],
-	[tc.DownloadFile(45, '/p7_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]), Send, WaitMode.Wait],
-	[tc.DownloadFile(46, '/p7_128_0', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]), Send, WaitMode.Wait],
-	[tc.DownloadFile(47, '/p8_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]), Send, WaitMode.Wait],
-	[tc.DownloadFile(48, '/p8_128_0', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]), Send, WaitMode.Wait],
-	[tc.DownloadFile(49, '/p9_128_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]), Send, WaitMode.Wait],
-	[tc.DownloadFile(50, '/p9_128_0', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]), Send, WaitMode.Wait],
-	[tc.DownloadFile(51, '/p1_480_0', [2, 3, 4, 7, 9, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]), Send, WaitMode.Wait],
-	[tc.DownloadFile(52, '/p1_480_0', [28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 43, 44, 45, 46, 47, 48]), Send, WaitMode.Wait],
-	[tc.DownloadFile(53, '/p1_480_0', [49, 50, 51, 52, 53, 54, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 67, 68, 69, 70]), Send, WaitMode.Wait],
-	[tc.DownloadFile(54, '/p1_480_0', [71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89]), Send, WaitMode.Wait]
+	[tc.DownloadFile(31, '/p5_128_0', [16, 17]), Send, WaitMode.Wait]
 ]
