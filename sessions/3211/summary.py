0c339209db8c31de169e8ade4ca8507800b7e639
begreeen <michal.gumiela@gmail.com>
2020-04-14 22:47:57 +0200

    summary for suns

--- sessions/3211/summary.py
+++ sessions/3211/summary.py
@@ -6,3 +6,8 @@ for p in range(1, 11):
 
     extract_file('p{}_480_0'.format(p), also=PHOTO_SESSIONS)
     decode_photo('assembled/p{}_480_0'.format(p), output_file_name='assembled/p{}_480_0.jpg'.format(p))
+
+
+
+SUNS_SESSIONS = [3207, 3208, 3210]
+extract_file('suns_ps_17', also=SUNS_SESSIONS)
