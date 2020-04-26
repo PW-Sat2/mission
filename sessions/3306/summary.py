13c5482a36f74a97495bf0b35fc3933b025138b6
Marek Antoniak <kfazol@gmail.com>
2020-04-26 12:07:41 +0200

    session-3306

---
+++ sessions/3306/summary.py
@@ -0,0 +1,16 @@
+PHOTO_SESSIONS = [3299, 3300, 3301, 3302, 3303, 3304, 3305]
+
+for p in range(1, 5):
+    extract_file('t{}n_480_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/t{}n_480_0'.format(p), output_file_name='assembled/t{}n_480_0.jpg'.format(p))
+
+    extract_file('t{}w_480_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/t{}w_480_0'.format(p), output_file_name='assembled/t{}w_480_0.jpg'.format(p))
+
+p=1
+
+extract_file('a{}n_480_0'.format(p), also=PHOTO_SESSIONS)
+decode_photo('assembled/a{}n_480_0'.format(p), output_file_name='assembled/a{}n_480_0.jpg'.format(p))
+
+extract_file('a{}w_480_0'.format(p), also=PHOTO_SESSIONS)
+decode_photo('assembled/a{}w_480_0'.format(p), output_file_name='assembled/a{}w_480_0.jpg'.format(p))
