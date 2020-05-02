8c64f8517d96f04ad459fa4f7b8d1093126422b8
alicjamusial <alicja@musial.pro>
2020-05-02 12:16:45 +0200

    Session 3346

---
+++ sessions/3346/summary.py
@@ -0,0 +1,8 @@
+PHOTO_SESSIONS = [3343, 3344, 3345]
+
+for p in range(1, 10):
+    extract_file('a{}n_480_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/a{}n_480_0'.format(p), output_file_name='assembled/a{}n_480_0.jpg'.format(p))
+
+    extract_file('a{}w_480_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/a{}w_480_0'.format(p), output_file_name='assembled/a{}w_480_0.jpg'.format(p))
