792007a471d4a62dd41dace153ed0f3125cb0e5e
alicjamusial <alicja@musial.pro>
2020-05-30 11:14:47 +0200

    Move bitrate to top

--- sessions/3527/tasklist.py
+++ sessions/3527/tasklist.py
@@ -1,10 +1,8 @@
 tasks = [
 
-    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
-
-    [tc.PingTelecommand(), Send, WaitMode.Wait],
-
     [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
+
+    [[tc.SendBeacon(), 10], SendLoop, WaitMode.NoWait],
 
     [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
 
