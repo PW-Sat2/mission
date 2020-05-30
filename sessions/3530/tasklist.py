2c5c8e4ef9380433d71b85ddec9dab01e6668738
alicjamusial <alicja@musial.pro>
2020-05-30 22:04:08 +0200

    Move single chunk to front

--- sessions/3530/tasklist.py
+++ sessions/3530/tasklist.py
@@ -17,6 +17,9 @@ tasks = [
 
 
     # missing from previous session start
+
+    [tc.DownloadFile(29, '/a3n_480_12_18_0', [88]), Send, WaitMode.Wait],
+
     [tc.DownloadFile(30, '/a10n_480_12_25_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]), Send, WaitMode.Wait],
     [tc.DownloadFile(31, '/a10w_480_12_25_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]), Send, WaitMode.Wait],
     [tc.DownloadFile(32, '/a5n_480_12_20_0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]), Send, WaitMode.Wait],
@@ -41,7 +44,6 @@ tasks = [
     [tc.DownloadFile(51, '/a10w_480_12_25_0', [108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125]), Send, WaitMode.Wait],
     [tc.DownloadFile(52, '/a10w_480_12_25_0', [126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143]), Send, WaitMode.Wait],
     [tc.DownloadFile(53, '/a10w_480_12_25_0', [144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160]), Send, WaitMode.Wait],
-    [tc.DownloadFile(54, '/a3n_480_12_18_0', [88]), Send, WaitMode.Wait],
     [tc.DownloadFile(55, '/a4n_480_12_19_0', [38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56]), Send, WaitMode.Wait],
     [tc.DownloadFile(56, '/a4n_480_12_19_0', [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]), Send, WaitMode.Wait],
     [tc.DownloadFile(57, '/a4n_480_12_19_0', [76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94]), Send, WaitMode.Wait],
