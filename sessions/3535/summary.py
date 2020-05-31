39c24f4498b2a03305f82613baf7fe10ac30f9c9
Łukasz Żak <zaklukasz32@gmail.com>
2020-05-31 13:56:08 +0200

    Session 3535

---
+++ sessions/3535/summary.py
@@ -0,0 +1,98 @@
+PHOTO_SESSIONS = [3528, 3529, 3530, 3531, 3533, 3534]
+
+extract_file('a1w_480_12_16_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a1w_480_12_16_0', output_file_name='assembled/a1w_480_12_16_0.jpg')
+
+extract_file('a1n_480_12_16_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a1n_480_12_16_0', output_file_name='assembled/a1n_480_12_16_0.jpg')
+
+
+extract_file('a2w_480_12_17_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a2w_480_12_17_0', output_file_name='assembled/a2w_480_12_17_0.jpg')
+
+extract_file('a2n_480_12_17_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a2n_480_12_17_0', output_file_name='assembled/a2n_480_12_17_0.jpg')
+
+
+extract_file('a3w_480_12_18_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a3w_480_12_18_0', output_file_name='assembled/a3w_480_12_18_0.jpg')
+
+extract_file('a3n_480_12_18_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a3n_480_12_18_0', output_file_name='assembled/a3n_480_12_18_0.jpg')
+
+
+extract_file('a4w_480_12_19_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a4w_480_12_19_0', output_file_name='assembled/a4w_480_12_19_0.jpg')
+
+extract_file('a4n_480_12_19_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a4n_480_12_19_0', output_file_name='assembled/a4n_480_12_19_0.jpg')
+
+
+extract_file('a5w_480_12_20_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a5w_480_12_20_0', output_file_name='assembled/a5w_480_12_20_0.jpg')
+
+extract_file('a5n_480_12_20_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a5n_480_12_20_0', output_file_name='assembled/a5n_480_12_20_0.jpg')
+
+
+extract_file('a6w_480_12_21_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a6w_480_12_21_0', output_file_name='assembled/a6w_480_12_21_0.jpg')
+
+extract_file('a6n_480_12_21_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a6n_480_12_21_0', output_file_name='assembled/a6n_480_12_21_0.jpg')
+
+
+extract_file('a7w_480_12_22_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a7w_480_12_22_0', output_file_name='assembled/a7w_480_12_22_0.jpg')
+
+extract_file('a7n_480_12_22_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a7n_480_12_22_0', output_file_name='assembled/a7n_480_12_22_0.jpg')
+
+
+extract_file('a8w_480_12_23_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a8w_480_12_23_0', output_file_name='assembled/a8w_480_12_23_0.jpg')
+
+extract_file('a8n_480_12_23_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a8n_480_12_23_0', output_file_name='assembled/a8n_480_12_23_0.jpg')
+
+
+extract_file('a9w_480_12_24_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a9w_480_12_24_0', output_file_name='assembled/a9w_480_12_24_0.jpg')
+
+extract_file('a9n_480_12_24_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a9n_480_12_24_0', output_file_name='assembled/a9n_480_12_24_0.jpg')
+
+
+extract_file('a10w_480_12_25_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a10w_480_12_25_0', output_file_name='assembled/a10w_480_12_25_0.jpg')
+
+extract_file('a10n_480_12_25_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a10n_480_12_25_0', output_file_name='assembled/a10n_480_12_25_0.jpg')
+
+
+extract_file('a11w_480_21_46_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a11w_480_21_46_0', output_file_name='assembled/a11w_480_21_46_0.jpg')
+
+extract_file('a11n_480_21_46_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a11n_480_21_46_0', output_file_name='assembled/a11n_480_21_46_0.jpg')
+
+
+extract_file('a12w_480_21_47_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a12w_480_21_47_0', output_file_name='assembled/a12w_480_21_47_0.jpg')
+
+extract_file('a12n_480_21_47_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a12n_480_21_47_0', output_file_name='assembled/a12n_480_21_47_0.jpg')
+
+
+extract_file('a13w_480_21_48_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a13w_480_21_48_0', output_file_name='assembled/a13w_480_21_48_0.jpg')
+
+extract_file('a13n_480_21_48_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a13n_480_21_48_0', output_file_name='assembled/a13n_480_21_48_0.jpg')
+
+
+extract_file('a14w_480_21_49_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a14w_480_21_49_0', output_file_name='assembled/a14w_480_21_49_0.jpg')
+
+extract_file('a14n_480_21_49_0', also=PHOTO_SESSIONS)
+decode_photo('assembled/a14n_480_21_49_0', output_file_name='assembled/a14n_480_21_49_0.jpg')
---
+++ sessions/3535/tasklist.py
@@ -0,0 +1,95 @@
+tasks = [
+    # Generated on 2020-05-31 13:52:23.621002, contains telemetry from sessions 3534 to 3535.
+    # The session starts on 2020-05-31 21:14:20+02:00.
+
+    [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
+
+    [[tc.SendBeacon(), 10], SendLoop, WaitMode.NoWait],
+
+    [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
+
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    # auto-generated telemetry start
+    [tc.DownloadFile(72, '/telemetry.current', [693, 743, 793, 843, 893, 943, 993, 1043, 1093, 1143, 1193, 1243, 1293, 1343, 1393, 1443, 1493, 1543, 718, 768]), Send, WaitMode.Wait],
+    [tc.DownloadFile(73, '/telemetry.current', [818, 868, 918, 968, 1018, 1068, 1118, 1168, 1218, 1268, 1318, 1368, 1418, 1468, 1518, 1568, 706, 756, 806, 856]), Send, WaitMode.Wait],
+    [tc.DownloadFile(74, '/telemetry.current', [906, 956, 1006, 1056, 1106, 1156, 1206, 1256, 1306, 1356, 1406, 1456, 1506, 1556, 730, 780, 830, 880, 930, 980]), Send, WaitMode.Wait],
+    [tc.DownloadFile(75, '/telemetry.current', [1030, 1080, 1130, 1180, 1230, 1280, 1330, 1380, 1430, 1480, 1530, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100]), Send, WaitMode.Wait],
+    [tc.DownloadFile(76, '/telemetry.current', [1150, 1200, 1250, 1300, 1350, 1400, 1450, 1500, 1550, 712, 762, 812, 862, 912, 962, 1012, 1062, 1112, 1162, 1212]), Send, WaitMode.Wait],
+    [tc.DownloadFile(77, '/telemetry.current', [1262, 1312, 1362, 1412, 1462, 1512, 1562, 724, 774, 824, 874, 924, 974, 1024, 1074, 1124, 1174, 1224, 1274, 1324]), Send, WaitMode.Wait],
+    [tc.DownloadFile(78, '/telemetry.current', [1374, 1424, 1474, 1524, 1574, 736, 786, 836, 886, 936, 986, 1036, 1086, 1136, 1186, 1236, 1286, 1336, 1386, 1436]), Send, WaitMode.Wait],
+    [tc.DownloadFile(79, '/telemetry.current', [1486, 1536]), Send, WaitMode.Wait],
+    # auto-generated telemetry end
+
+
+    # missing from previous session start
+    [tc.DownloadFile(30, '/a5w_480_12_20_0', [57, 59, 60, 61, 64, 66, 67, 68, 69, 72, 73, 74, 75]), Send, WaitMode.Wait],
+    [tc.DownloadFile(31, '/a5w_480_12_20_0', [76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87]), Send, WaitMode.Wait],
+    [tc.DownloadFile(32, '/a6n_480_12_21_0', [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38]), Send, WaitMode.Wait],
+    [tc.DownloadFile(33, '/a6n_480_12_21_0', [39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57]), Send, WaitMode.Wait],
+    [tc.DownloadFile(34, '/a6n_480_12_21_0', [58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76]), Send, WaitMode.Wait],
+    [tc.DownloadFile(35, '/a6w_480_12_21_0', [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39]), Send, WaitMode.Wait],
+    [tc.DownloadFile(36, '/a6w_480_12_21_0', [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]), Send, WaitMode.Wait],
+    [tc.DownloadFile(37, '/a6w_480_12_21_0', [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79]), Send, WaitMode.Wait],
+    [tc.DownloadFile(38, '/a6w_480_12_21_0', [80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98]), Send, WaitMode.Wait],
+    [tc.DownloadFile(39, '/a6w_480_12_21_0', [99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117]), Send, WaitMode.Wait],
+    [tc.DownloadFile(40, '/a6w_480_12_21_0', [118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136]), Send, WaitMode.Wait],
+    [tc.DownloadFile(41, '/a6w_480_12_21_0', [137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155]), Send, WaitMode.Wait],
+    [tc.DownloadFile(42, '/a7n_480_12_22_0', [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]), Send, WaitMode.Wait],
+    [tc.DownloadFile(43, '/a7n_480_12_22_0', [35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51]), Send, WaitMode.Wait],
+    [tc.DownloadFile(44, '/a7n_480_12_22_0', [52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68]), Send, WaitMode.Wait],
+    [tc.DownloadFile(45, '/a7n_480_12_22_0', [69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85]), Send, WaitMode.Wait],
+    [tc.DownloadFile(46, '/a7w_480_12_22_0', [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37]), Send, WaitMode.Wait],
+    [tc.DownloadFile(47, '/a7w_480_12_22_0', [38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56]), Send, WaitMode.Wait],
+    [tc.DownloadFile(48, '/a7w_480_12_22_0', [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]), Send, WaitMode.Wait],
+    [tc.DownloadFile(49, '/a7w_480_12_22_0', [76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94]), Send, WaitMode.Wait],
+    [tc.DownloadFile(50, '/a7w_480_12_22_0', [95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112]), Send, WaitMode.Wait],
+    [tc.DownloadFile(51, '/a8n_480_12_23_0', [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]), Send, WaitMode.Wait],
+    [tc.DownloadFile(52, '/a8n_480_12_23_0', [35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51]), Send, WaitMode.Wait],
+    [tc.DownloadFile(53, '/a8n_480_12_23_0', [52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68]), Send, WaitMode.Wait],
+    [tc.DownloadFile(54, '/a8n_480_12_23_0', [69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85]), Send, WaitMode.Wait],
+    [tc.DownloadFile(55, '/a8w_480_12_23_0', [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]), Send, WaitMode.Wait],
+    [tc.DownloadFile(56, '/a8w_480_12_23_0', [34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]), Send, WaitMode.Wait],
+    [tc.DownloadFile(57, '/a8w_480_12_23_0', [51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67]), Send, WaitMode.Wait],
+    [tc.DownloadFile(58, '/a8w_480_12_23_0', [68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84]), Send, WaitMode.Wait],
+    [tc.DownloadFile(59, '/a8w_480_12_23_0', [85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]), Send, WaitMode.Wait],
+    [tc.DownloadFile(60, '/a9n_480_12_24_0', [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37]), Send, WaitMode.Wait],
+    [tc.DownloadFile(61, '/a9n_480_12_24_0', [38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56]), Send, WaitMode.Wait],
+    [tc.DownloadFile(62, '/a9n_480_12_24_0', [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]), Send, WaitMode.Wait],
+    [tc.DownloadFile(63, '/a9n_480_12_24_0', [76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94]), Send, WaitMode.Wait],
+    [tc.DownloadFile(64, '/a9w_480_12_24_0', [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37]), Send, WaitMode.Wait],
+    [tc.DownloadFile(65, '/a9w_480_12_24_0', [38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56]), Send, WaitMode.Wait],
+    [tc.DownloadFile(66, '/a9w_480_12_24_0', [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]), Send, WaitMode.Wait],
+    [tc.DownloadFile(67, '/a9w_480_12_24_0', [76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94]), Send, WaitMode.Wait],
+    [tc.DownloadFile(68, '/a9w_480_12_24_0', [95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112]), Send, WaitMode.Wait],
+    [tc.DownloadFile(69, '/a9w_480_12_24_0', [113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130]), Send, WaitMode.Wait],
+    [tc.DownloadFile(70, '/a9w_480_12_24_0', [131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148]), Send, WaitMode.Wait],
+    [tc.DownloadFile(71, '/a9w_480_12_24_0', [149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166]), Send, WaitMode.Wait],
+    # missing from previous session end
+
+
+    # auto-generated file download start
+
+    # auto-generated file download end
+
+
+    # auto-generated file remove start
+
+    # auto-generated file remove end
+
+    # When everything is downloaded, switch to deep-sleep
+    # Carefully waste some session time
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    ["Set bootslots for deep_sleep.", Print, WaitMode.NoWait],
+    [tc.SetBootSlots(103, 0b111000, 0b111), Send, WaitMode.Wait],
+
+    # Wait for good uplink before power cycle
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    # Power cycle EPS B
+    [tc.PowerCycleTelecommand(6), Send, WaitMode.Wait],
+
+    # Get beacons from deep-sleep
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
+]
