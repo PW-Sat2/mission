83cbddcb37758351aaff886769d87f84b35c4b6c
begreeen <michal.gumiela@gmail.com>
2020-06-13 14:59:16 +0200

    session 3619

---
+++ sessions/3619/summary.py
@@ -0,0 +1,7 @@
+PHOTO_SESSIONS = [3617]
+
+photos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for p in photos:
+    extract_file('p{}_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/p{}_0'.format(p), output_file_name='assembled/p{}_0.jpg'.format(p))
