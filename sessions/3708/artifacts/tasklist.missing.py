b0a6b6017a3bd89c1368d8113f1db620f8f9f9ad
operator <sat_operator@future-processing.com>
2020-06-27 22:55:13 +0200

    3708 - summary

---
+++ sessions/3708/artifacts/tasklist.missing.py
@@ -0,0 +1,16 @@
+tasks = [
+[tc.DownloadFile(30, '/a_n_17_00_0', [47]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/a_n_17_02_0', [49]), Send, WaitMode.Wait],
+	[tc.DownloadFile(32, '/a_n_17_03_0', [43, 44]), Send, WaitMode.Wait],
+	[tc.DownloadFile(33, '/a_n_20_02_0', [34]), Send, WaitMode.Wait],
+	[tc.DownloadFile(34, '/a_w_17_00_0', [57, 58, 59]), Send, WaitMode.Wait],
+	[tc.DownloadFile(35, '/a_w_17_01_0', [27, 45, 59]), Send, WaitMode.Wait],
+	[tc.DownloadFile(36, '/a_w_17_02_0', [31, 45]), Send, WaitMode.Wait],
+	[tc.DownloadFile(37, '/a_w_17_03_0', [13, 16, 17, 19, 30, 43, 44, 45, 46]), Send, WaitMode.Wait],
+	[tc.DownloadFile(38, '/a_w_17_04_0', [15, 20, 22, 30, 54]), Send, WaitMode.Wait],
+	[tc.DownloadFile(39, '/a_w_20_02_0', [54, 57]), Send, WaitMode.Wait],
+	[tc.DownloadFile(40, '/a_w_20_03_0', [47, 48, 49]), Send, WaitMode.Wait],
+	[tc.DownloadFile(41, '/dummy_15_33_0', [20, 21, 22, 23, 24, 25, 27, 41, 42]), Send, WaitMode.Wait],
+	[tc.DownloadFile(42, '/dummy_16_33_0', [23, 26]), Send, WaitMode.Wait],
+	[tc.DownloadFile(43, '/dummy_19_04_0', [47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]), Send, WaitMode.Wait]
+]
