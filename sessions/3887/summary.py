c3b6971879960b654d787f3143687ac997824e1e
Piotr Kuligowski <xsaperx@gmail.com>
2020-07-25 13:43:43 +0200

    CR fix

---
+++ sessions/3887/summary.py
@@ -0,0 +1,22 @@
+PHOTO_SESSIONS = []
+
+files = [
+    'a_w_12_46_0',
+    'a_n_12_46_0',
+    'a_w_12_48_0',
+    'a_n_12_48_0',
+    'a_w_12_50_0',
+    'a_n_12_50_0',
+    'a_w_14_10_0',
+    'a_n_14_10_0',
+    'a_w_14_12_0',
+    'a_n_14_12_0',
+    'a_w_14_14_0',
+    'a_n_14_14_0',
+    'a_w_14_16_0',
+    'a_n_14_16_0',
+]
+
+for f in files:
+    extract_file(f, also=PHOTO_SESSIONS)
+    decode_photo('assembled/' + f, output_file_name='assembled/' + f + '.jpg')
