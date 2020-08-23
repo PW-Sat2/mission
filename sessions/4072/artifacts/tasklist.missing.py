ee1a3c0227aabc0d03a274389bce3a2a9777e1ca
operator <sat_operator@future-processing.com>
2020-08-23 11:23:36 +0200

    4072 - summary

---
+++ sessions/4072/artifacts/tasklist.missing.py
@@ -0,0 +1,20 @@
+tasks = [
+[tc.DownloadFile(30, '/telemetry.current', [23, 73, 123, 137, 173, 187, 237, 1206, 1280, 1294]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/telemetry.previous', [1403, 1473, 1510, 1560, 1903, 1917, 1979, 1991, 2085, 2167, 2179, 2185, 2235]), Send, WaitMode.Wait],
+	[tc.DownloadFile(32, '/a_w_11_49_0', [100]), Send, WaitMode.Wait],
+	[tc.DownloadFile(33, '/a_w_11_50_0', [22, 23, 24, 26, 27, 29, 31, 32, 33, 34, 38, 40, 41, 42, 43, 44, 45]), Send, WaitMode.Wait],
+	[tc.DownloadFile(34, '/a_w_11_50_0', [46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62]), Send, WaitMode.Wait],
+	[tc.DownloadFile(35, '/a_w_11_50_0', [63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 74, 77, 78, 89, 98, 99, 100]), Send, WaitMode.Wait],
+	[tc.DownloadFile(36, '/a_w_11_50_0', [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116]), Send, WaitMode.Wait],
+	[tc.DownloadFile(37, '/a_w_11_51_0', [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57]), Send, WaitMode.Wait],
+	[tc.DownloadFile(38, '/a_w_11_51_0', [58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]), Send, WaitMode.Wait],
+	[tc.DownloadFile(39, '/a_w_11_51_0', [76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93]), Send, WaitMode.Wait],
+	[tc.DownloadFile(40, '/a_w_11_51_0', [94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110]), Send, WaitMode.Wait],
+	[tc.DownloadFile(41, '/a_w_11_51_0', [111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127]), Send, WaitMode.Wait],
+	[tc.DownloadFile(42, '/a_w_12_59_0', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]), Send, WaitMode.Wait],
+	[tc.DownloadFile(43, '/a_w_12_59_0', [30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44]), Send, WaitMode.Wait],
+	[tc.DownloadFile(44, '/a_w_12_59_0', [45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]), Send, WaitMode.Wait],
+	[tc.DownloadFile(45, '/a_w_13_00_0', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]), Send, WaitMode.Wait],
+	[tc.DownloadFile(46, '/a_w_13_00_0', [30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44]), Send, WaitMode.Wait],
+	[tc.DownloadFile(47, '/a_w_13_00_0', [45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]), Send, WaitMode.Wait]
+]
