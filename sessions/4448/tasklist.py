cb152238494e0c8b8f8af27bcf74ccc6f49b3481
Marek Antoniak <kfazol@gmail.com>
2020-10-21 00:36:37 +0200

    Session #4448

---
+++ sessions/4448/tasklist.py
@@ -0,0 +1,16 @@
+tasks = [
+    [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
+
+    [[tc.SendBeacon(), 10], SendLoop, WaitMode.NoWait],
+
+    ["The next step is SetBootSlots.", Print, WaitMode.Wait],
+    [tc.SetBootSlots(76, 0b111000, 0b111), Send, WaitMode.Wait],
+
+    ["The next step is Power Cycle B.", Print, WaitMode.Wait],
+    [tc.PowerCycleTelecommand(100), Send, WaitMode.Wait],
+    
+    ["The satellite shall boot to the new software.", Print, WaitMode.Wait],
+    [tc.PingTelecommand(), Send, WaitMode.Wait],
+
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
+]
