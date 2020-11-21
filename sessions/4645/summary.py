6182b46266027ea9342adb419be2dc7c7397f0a2
Maciej Nowak <maciejt.nowak@gmail.com>
2020-11-21 14:08:56 +0100

    Session 4645

---
+++ sessions/4645/summary.py
@@ -0,0 +1,10 @@
+PHOTO_SESSIONS = [4644, 4643]
+
+photos = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+for p in photos:
+    extract_file('n_w_{}_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/n_w_{}_0'.format(p), output_file_name='assembled/n_w_{}_0.jpg'.format(p))
+
+    extract_file('n_n_{}_0'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/n_n_{}_0'.format(p), output_file_name='assembled/n_n_{}_0.jpg'.format(p))
