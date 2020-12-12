f27cd135c18735b1b6f4181f6175dff172c9d933
operator <sat_operator@future-processing.com>
2020-12-12 13:44:34 +0100

    4775 - summary

---
+++ sessions/4775/artifacts/tasklist.missing.py
@@ -0,0 +1,10 @@
+tasks = [
+[tc.DownloadFile(30, '/b_n_p480_1_0', [80]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/b_n_p480_2_0', [80, 81, 82, 83]), Send, WaitMode.Wait],
+	[tc.DownloadFile(32, '/b_w_p480_1_0', [80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97]), Send, WaitMode.Wait],
+	[tc.DownloadFile(33, '/b_w_p480_1_0', [98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114]), Send, WaitMode.Wait],
+	[tc.DownloadFile(34, '/b_w_p480_1_0', [115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131]), Send, WaitMode.Wait],
+	[tc.DownloadFile(35, '/b_w_p480_1_0', [132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148]), Send, WaitMode.Wait],
+	[tc.DownloadFile(36, '/b_w_p480_1_0', [149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165]), Send, WaitMode.Wait],
+	[tc.DownloadFile(37, '/b_w_p480_2_0', [80, 81, 82, 83, 84, 85, 86, 87]), Send, WaitMode.Wait]
+]
