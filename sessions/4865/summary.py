bb61942fde447c677ebc0c8ff2dc2688ecb691ff
Maciej Nowak <maciejt.nowak@gmail.com>
2020-12-26 23:44:17 +0100

    Session 4865

---
+++ sessions/4865/summary.py
@@ -0,0 +1,10 @@
+PHOTO_SESSIONS = list(range(4861, session.session_number))
+
+photos = ['a_w_p480_11_22_0', 'a_n_p480_11_22_0', 'a_w_p480_11_26_0', 'a_n_p480_11_26_0', 'a_w_p480_11_32_0',
+          'a_n_p480_11_32_0', 'a_w_p480_11_40_0', 'a_n_p480_11_40_0', 'a_w_p480_11_52_0', 'a_n_p480_11_52_0',
+          'a_w_p480_11_56_0', 'a_n_p480_11_56_0', 'a_w_p480_12_00_0', 'a_n_p480_12_00_0', 'a_w_p480_12_04_0',
+          'a_n_p480_12_04_0']
+
+for p in photos:
+    extract_file('{}'.format(p), also=PHOTO_SESSIONS)
+    decode_photo('assembled/{}'.format(p), output_file_name='assembled/{}.jpg'.format(p))
