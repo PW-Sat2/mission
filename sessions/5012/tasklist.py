9f4fa4173c573af5813f4f557cb7c340521b5d4d
alicjamusial <alicja@musial.pro>
2021-01-20 12:46:40 +0100

    Session 5012

---
+++ sessions/5012/tasklist.py
@@ -0,0 +1,77 @@
+tasks = [
+    # Generated on 2021-01-20 12:42:52.138000, contains telemetry from sessions 5011 to 5012.
+    # The session starts on 2021-01-20 13:20:50+01:00.
+
+    [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
+
+    [[tc.SendBeacon(), 10], SendLoop, WaitMode.NoWait],
+
+    [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
+
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    # auto-generated telemetry start
+
+    # auto-generated telemetry end
+
+
+    # missing from previous session start
+    [tc.DownloadFile(30, '/telemetry.current', [3, 15, 27, 39, 65, 77, 89, 109, 115, 127, 139, 153, 165, 177, 189, 203]), Send, WaitMode.Wait],
+    [tc.DownloadFile(31, '/telemetry.current', [215, 227, 239, 253, 353, 365, 377, 383, 389, 439, 443, 475, 493, 525, 575]), Send, WaitMode.Wait],
+    [tc.DownloadFile(32, '/telemetry.previous', [1807, 1819, 1919, 1995, 2007, 2269]), Send, WaitMode.Wait],
+    [tc.DownloadFile(33, '/p03w_240_1', [42]), Send, WaitMode.Wait],
+    [tc.DownloadFile(34, '/p03w_240_13', [24]), Send, WaitMode.Wait],
+    [tc.DownloadFile(35, '/p03w_240_15', [20, 21, 22, 30]), Send, WaitMode.Wait],
+    [tc.DownloadFile(36, '/p03w_240_17', [20, 21, 22, 29, 30, 31, 32, 44, 45, 46, 47, 48, 49, 51, 52, 60]), Send, WaitMode.Wait],
+    [tc.DownloadFile(37, '/p03w_240_17', [61, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81]), Send, WaitMode.Wait],
+    [tc.DownloadFile(38, '/p03w_240_17', [82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96]), Send, WaitMode.Wait],
+    [tc.DownloadFile(39, '/p03w_240_19', [20, 22, 24, 25, 26, 27, 30, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49]), Send, WaitMode.Wait],
+    [tc.DownloadFile(40, '/p03w_240_19', [50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65]), Send, WaitMode.Wait],
+    [tc.DownloadFile(41, '/p03w_240_19', [66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81]), Send, WaitMode.Wait],
+    [tc.DownloadFile(42, '/p03w_240_19', [82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97]), Send, WaitMode.Wait],
+    [tc.DownloadFile(43, '/p03w_240_21', [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]), Send, WaitMode.Wait],
+    [tc.DownloadFile(44, '/p03w_240_23', [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]), Send, WaitMode.Wait],
+    [tc.DownloadFile(45, '/p03w_240_25', [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]), Send, WaitMode.Wait],
+    [tc.DownloadFile(46, '/p03w_240_25', [27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39]), Send, WaitMode.Wait],
+    [tc.DownloadFile(47, '/p03w_240_27', [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37]), Send, WaitMode.Wait],
+    [tc.DownloadFile(48, '/p03w_240_27', [38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56]), Send, WaitMode.Wait],
+    [tc.DownloadFile(49, '/p03w_240_27', [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]), Send, WaitMode.Wait],
+    [tc.DownloadFile(50, '/p03w_240_3', [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]), Send, WaitMode.Wait],
+    [tc.DownloadFile(51, '/p03w_240_3', [35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51]), Send, WaitMode.Wait],
+    [tc.DownloadFile(52, '/p03w_240_3', [52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68]), Send, WaitMode.Wait],
+    [tc.DownloadFile(53, '/p03w_240_3', [69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85]), Send, WaitMode.Wait],
+    [tc.DownloadFile(54, '/p03w_240_5', [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]), Send, WaitMode.Wait],
+    [tc.DownloadFile(55, '/p03w_240_7', [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]), Send, WaitMode.Wait],
+    [tc.DownloadFile(56, '/p03w_240_9', [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]), Send, WaitMode.Wait],
+    [tc.DownloadFile(57, '/p03w_240_9', [34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]), Send, WaitMode.Wait],
+    [tc.DownloadFile(58, '/p03w_240_9', [51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66]), Send, WaitMode.Wait],
+    [tc.DownloadFile(59, '/p04w_240_0', [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]), Send, WaitMode.Wait],
+    [tc.DownloadFile(60, '/p04w_240_0', [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47]), Send, WaitMode.Wait],
+    [tc.DownloadFile(61, '/p04w_240_0', [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62]), Send, WaitMode.Wait],
+    [tc.DownloadFile(62, '/p04w_240_2', [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]), Send, WaitMode.Wait],
+    [tc.DownloadFile(63, '/p04w_240_2', [34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]), Send, WaitMode.Wait],
+    [tc.DownloadFile(64, '/p04w_240_2', [51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67]), Send, WaitMode.Wait],
+    [tc.DownloadFile(65, '/p04w_240_2', [68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83]), Send, WaitMode.Wait],
+    [tc.DownloadFile(66, '/p04w_240_2', [84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]), Send, WaitMode.Wait],
+    [tc.DownloadFile(67, '/p04w_240_4', [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]), Send, WaitMode.Wait],
+    [tc.DownloadFile(68, '/p04w_240_4', [28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]), Send, WaitMode.Wait],
+    [tc.DownloadFile(69, '/p04w_240_6', [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]), Send, WaitMode.Wait],
+    # missing from previous session end
+
+    # Carefully waste some session time
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    ["Set bootslots for deep_sleep.", Print, WaitMode.NoWait],
+    [tc.SetBootSlots(200, 0b111000, 0b111), Send, WaitMode.Wait],
+
+    # Wait for good uplink before power cycle
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    # Power cycle EPS B
+    [tc.PowerCycleTelecommand(3), Send, WaitMode.Wait],
+
+    # Get beacons from deep-sleep
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
+
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
+]
