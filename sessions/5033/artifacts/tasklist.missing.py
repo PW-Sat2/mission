78fd825d5afed9f7759b8aefb7897b8e0d742c04
operator <sat_operator@future-processing.com>
2021-01-24 12:24:10 +0100

    5033 - summary

---
+++ sessions/5033/artifacts/tasklist.missing.py
@@ -0,0 +1,4 @@
+tasks = [
+[tc.DownloadFile(30, '/telemetry.current', [857, 870, 882, 957, 1007, 1020, 1057]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/p3_w_p480_0', [129, 130, 131, 132]), Send, WaitMode.Wait]
+]
