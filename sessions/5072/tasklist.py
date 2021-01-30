cc2d55d522109c350cd88af631b1c2939b02f53f
Grzegorz Gajoch <gajoch@gmail.com>
2021-01-30 22:43:16 +0100

    Go to sleep

--- sessions/5072/tasklist.py
+++ sessions/5072/tasklist.py
@@ -10,37 +10,25 @@ tasks = [
 
     [tc.SendBeacon(), Send, WaitMode.Wait],
 
-    # radfet_58
-    [tc.PerformRadFETExperiment(100, 150, 110, 'radfet_58'), Send, WaitMode.Wait],
-
-    # foteczki (11:04), zakladamy wyslanie o 11:00
-    [tc.TakePhotoTelecommand(4, CameraLocation.Wing, PhotoResolution.p480, 1, datetime.timedelta(minutes=17), 'p480_w'), Send, WaitMode.Wait],
-    [tc.TakePhotoTelecommand(5, CameraLocation.Nadir, PhotoResolution.p480, 1, datetime.timedelta(minutes=0), 'p480_n'), Send, WaitMode.Wait],
-
-    [tc.TakePhotoTelecommand(6, CameraLocation.Wing, PhotoResolution.p480, 1, datetime.timedelta(minutes=2), 'p480_w_2'), Send, WaitMode.Wait],
-    [tc.TakePhotoTelecommand(7, CameraLocation.Nadir, PhotoResolution.p480, 1, datetime.timedelta(minutes=0), 'p480_n_2'), Send, WaitMode.Wait],
+    ["Set bootslots for deep_sleep.", Print, WaitMode.NoWait],
+    [tc.SetBootSlots(200, 0b111000, 0b111), Send, WaitMode.Wait],
 
     # auto-generated telemetry start
     [tc.DownloadFile(32, '/telemetry.current', [448, 498, 548, 598, 648, 473, 523, 573, 623, 461, 511, 561, 611, 661, 485, 535, 585, 635, 455, 505]), Send, WaitMode.Wait],
     [tc.DownloadFile(33, '/telemetry.current', [555, 605, 655, 467, 517, 567, 617, 479, 529, 579, 629, 491, 541, 591, 641]), Send, WaitMode.Wait],
     # auto-generated telemetry end
 
-
     # missing from previous session start
     [tc.DownloadFile(30, '/telemetry.current', [14, 314]), Send, WaitMode.Wait],
     [tc.DownloadFile(31, '/telemetry.previous', [294, 644, 2044, 2094, 2144, 2194, 2244]), Send, WaitMode.Wait],
     # missing from previous session end
 
+    # Wait for good uplink before power cycle
+    [tc.SendBeacon(), Send, WaitMode.Wait],
 
-    # auto-generated file download start
-    
-    # auto-generated file download end
+    # Power cycle EPS B
+    [tc.PowerCycleTelecommand(3), Send, WaitMode.Wait],
 
-
-    # auto-generated file remove start
-    
-    # auto-generated file remove end
-
-
+    # Get beacons from deep-sleep
     [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
 ]
