98f29d9aeff8ee6beedd927df336c7ba839ddc60
Michał Gumiela <michal.gumiela@gmail.com>
2021-02-16 22:27:51 +0100

    session 5170

---
+++ sessions/5170/tasklist.py
@@ -0,0 +1,25 @@
+tasks = [
+
+    [[tc.SendBeacon(), 10], SendLoop, WaitMode.NoWait],
+
+    [tc.little_oryx.RebootToNormal(), Send, WaitMode.Wait],
+
+    [tc.PingTelecommand(), Send, WaitMode.Wait],
+
+    [[tc.SetBitrate(1, BaudRate.BaudRate9600), 5], SendLoop, WaitMode.NoWait],
+
+    [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
+
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    [tc.PerformRadFETExperiment(100, 150, 110, 'radfet_60'), Send, WaitMode.Wait],
+
+    [tc.TakePhotoTelecommand(4, CameraLocation.Nadir, PhotoResolution.p480, 1, datetime.timedelta(minutes=30), 'p0_n_p480'), Send, WaitMode.Wait],
+    [tc.TakePhotoTelecommand(6, CameraLocation.Wing, PhotoResolution.p480, 1, datetime.timedelta(minutes=1), 'p1_w_p480'), Send, WaitMode.Wait],
+
+    [tc.ListFiles(2, '/'), Send, WaitMode.Wait],
+
+    [tc.SendBeacon(), Send, WaitMode.Wait],
+
+    [[tc.SendBeacon(), 20], SendLoop, WaitMode.NoWait],
+]
