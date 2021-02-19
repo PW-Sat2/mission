0a89ffa5c1c7ad8c1552d799d9ed43a99134dfc1
Piotr Kuligowski <xsaperx@gmail.com>
2021-02-19 23:48:58 +0100

    summary fix

--- sessions/5186/summary.py
+++ sessions/5186/summary.py
@@ -1,6 +1,6 @@
 PHOTO_SESSIONS = list(range(5172, session.session_number))
 
-photos = ['p1_n_480', 'p1_w_480', 'p2_n_480', 'p2_w_480']
+photos = ['p1_n_480_0', 'p1_w_480_0', 'p2_n_480_0', 'p2_w_480_0']
 
 for p in photos:
     extract_file('{}'.format(p), also=PHOTO_SESSIONS)
