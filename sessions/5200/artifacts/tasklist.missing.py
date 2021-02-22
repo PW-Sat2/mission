09884f8fd9175b7738e02e380428a04deb442b00
operator <sat_operator@future-processing.com>
2021-02-22 12:43:01 +0100

    5200 - summary

---
+++ sessions/5200/artifacts/tasklist.missing.py
@@ -0,0 +1,15 @@
+tasks = [
+[tc.DownloadFile(30, '/telemetry.current', [729, 829]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/telemetry.previous', [2028, 2160]), Send, WaitMode.Wait],
+	[tc.DownloadFile(32, '/pw_p10_0', [28, 29, 30, 39, 52, 53, 54, 55, 56, 57, 58, 65, 66, 67, 68, 69]), Send, WaitMode.Wait],
+	[tc.DownloadFile(33, '/pw_p14_0', [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]), Send, WaitMode.Wait],
+	[tc.DownloadFile(34, '/pw_p14_0', [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47]), Send, WaitMode.Wait],
+	[tc.DownloadFile(35, '/pw_p14_0', [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62]), Send, WaitMode.Wait],
+	[tc.DownloadFile(36, '/pw_p19_0', [2, 4, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]), Send, WaitMode.Wait],
+	[tc.DownloadFile(37, '/pw_p19_0', [30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45]), Send, WaitMode.Wait],
+	[tc.DownloadFile(38, '/pw_p19_0', [46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]), Send, WaitMode.Wait],
+	[tc.DownloadFile(39, '/pw_p6_0', [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]), Send, WaitMode.Wait],
+	[tc.DownloadFile(40, '/pw_p6_0', [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47]), Send, WaitMode.Wait],
+	[tc.DownloadFile(41, '/pw_p6_0', [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63]), Send, WaitMode.Wait],
+	[tc.DownloadFile(42, '/pw_p6_0', [64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79]), Send, WaitMode.Wait]
+]
