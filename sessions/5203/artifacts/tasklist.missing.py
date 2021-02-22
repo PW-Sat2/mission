993bf5a04652974c632d2d9b275670af4597352e
operator <sat_operator@future-processing.com>
2021-02-22 22:56:02 +0100

    5203 - summary

---
+++ sessions/5203/artifacts/tasklist.missing.py
@@ -0,0 +1,19 @@
+tasks = [
+[tc.DownloadFile(30, '/telemetry.current', [82, 729, 893, 905, 917, 929, 943, 949, 955, 967, 979, 993, 1005, 1017, 1029, 1055, 1067, 1079, 1105, 1117]), Send, WaitMode.Wait],
+	[tc.DownloadFile(31, '/telemetry.current', [1129, 1155, 1167, 1179, 1205, 1217, 1229, 1243, 1255, 1267, 1279, 1293, 1305, 1317, 1329, 1343, 1355, 1367, 1379]), Send, WaitMode.Wait],
+	[tc.DownloadFile(32, '/telemetry.current', [1393, 1405, 1417, 1423, 1429, 1443, 1455, 1467, 1479, 1493, 1505, 1517, 1529, 1543, 1555, 1567, 1573, 1579, 1593]), Send, WaitMode.Wait],
+	[tc.DownloadFile(33, '/telemetry.current', [1605, 1617, 1623, 1629, 1643, 1655, 1667, 1679, 1693, 1705, 1717, 1729, 1743, 1755, 1767, 1779, 1793, 1805, 1817]), Send, WaitMode.Wait],
+	[tc.DownloadFile(34, '/telemetry.current', [1829, 1843, 1855, 1867, 1879, 1893, 1905, 1917, 1929, 1943, 1955, 1967, 1979, 1993, 2005, 2017, 2023, 2029, 2043]), Send, WaitMode.Wait],
+	[tc.DownloadFile(35, '/telemetry.previous', [2028, 2160]), Send, WaitMode.Wait],
+	[tc.DownloadFile(36, '/pw_p10_0', [28, 29, 30, 39, 52, 53, 54, 55, 56, 57, 58, 65, 66, 67, 68, 69]), Send, WaitMode.Wait],
+	[tc.DownloadFile(37, '/pw_p14_0', [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]), Send, WaitMode.Wait],
+	[tc.DownloadFile(38, '/pw_p14_0', [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47]), Send, WaitMode.Wait],
+	[tc.DownloadFile(39, '/pw_p14_0', [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62]), Send, WaitMode.Wait],
+	[tc.DownloadFile(40, '/pw_p19_0', [2, 4, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]), Send, WaitMode.Wait],
+	[tc.DownloadFile(41, '/pw_p19_0', [30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45]), Send, WaitMode.Wait],
+	[tc.DownloadFile(42, '/pw_p19_0', [46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]), Send, WaitMode.Wait],
+	[tc.DownloadFile(43, '/pw_p6_0', [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]), Send, WaitMode.Wait],
+	[tc.DownloadFile(44, '/pw_p6_0', [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47]), Send, WaitMode.Wait],
+	[tc.DownloadFile(45, '/pw_p6_0', [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63]), Send, WaitMode.Wait],
+	[tc.DownloadFile(46, '/pw_p6_0', [64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79]), Send, WaitMode.Wait]
+]
